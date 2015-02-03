jpsdr/MKVToolnix-Windows-7

commit 6e35edac137f31c0aa423f620487f23e5b2ec960

all: close redirected stdout before global destruction
If it's redirected to a file then this is an instance of a buffered file
writer. If it's only collected via global destruction after its
reference count reaches zero then flushing the buffers will call some
debugging option stuff which is invalid at that point.

src/extract/mkvextract.cpp
@@ -105,5 +105,5 @@ main(int argc,
   else
     usage(2);
 
-  return 0;
+  mxexit(0);
 }

src/info/mkvinfo.cpp
@@ -1759,7 +1759,9 @@ main(int argc,
   init_common_boost_formats();
 
   if (g_options.m_use_gui)
-    return ui_run(argc, argv);
+    ui_run(argc, argv);
   else
-    return console_main();
+    console_main();
+
+  mxexit(0);
 }

src/merge/mkvmerge.cpp
@@ -2511,5 +2511,5 @@ main(int argc,
 
   cleanup();
 
-  mxexit();
+  mxexit(0);
 }

src/mkvtoolnix-gui/mkvtoolnix_gui.cpp
@@ -22,5 +22,7 @@ main(int argc,
   MainWindow mainWindow;
   mainWindow.show();
 
-  return app.exec();
+  app.exec();
+
+  mxexit(0);
 }

src/propedit/propedit.cpp
@@ -152,5 +152,5 @@ main(int argc,
 
   run(options);
 
-  mxexit();
+  mxexit(0);
 }

src/tools/ac3parser.cpp
@@ -125,5 +125,5 @@ main(int argc,
     mxerror(Y("File not found\n"));
   }
 
-  return 0;
+  mxexit(0);
 }

src/tools/base64tool.cpp
@@ -122,5 +122,5 @@ main(int argc,
 
   mxinfo(Y("Done.\n"));
 
-  return 0;
+  mxexit(0);
 }

src/tools/diracparser.cpp
@@ -232,5 +232,5 @@ main(int argc,
     mxerror(Y("File not found\n"));
   }
 
-  return 0;
+  mxexit(0);
 }

commit 7362348340aba1884e3c8cb411ff5858f519c390

all tools: fix exit code if warnings occurred
If a warning occurred then the exit code must be 1, not 0. This has been
broken in commit 6e35edac137f31c0aa423f620487f23e5b2ec960 (since 7.0.0).

Fixes #1101.

src/common/command_line.cpp
@@ -257,7 +257,7 @@ handle_common_cli_args(std::vector<std::string> &args,
           mxinfo(boost::format("  %1% (%2%)\n") % translation->get_locale() % translation->m_english_name);
           ++translation;
         }
-        mxexit(0);
+        mxexit();
       }
 
       if (-1 == translation_c::look_up_translation(args[i + 1]))
@@ -277,7 +277,7 @@ handle_common_cli_args(std::vector<std::string> &args,
   while (args.size() > i) {
     if ((args[i] == "-V") || (args[i] == "--version")) {
       mxinfo(boost::format("%1%\n") % version_info);
-      mxexit(0);
+      mxexit();
 
     } else if ((args[i] == "-v") || (args[i] == "--verbose")) {
       ++verbose;

src/common/hacks.cpp
@@ -68,7 +68,7 @@ engage_hacks(const std::string &hacks) {
       mxinfo(Y("Valid hacks are:\n"));
       for (hidx = 0; s_available_hacks[hidx].name; ++hidx)
         mxinfo(boost::format("%1%\n") % s_available_hacks[hidx].name);
-      mxexit(0);
+      mxexit();
 
     } else if (engage_args[aidx] == "cow") {
       const std::string initial = "ICAgICAgICAgIChfXykKICAgICAgICAgICgqKikg"
@@ -79,7 +79,7 @@ engage_hacks(const std::string &hacks) {
       memset(correction, 0, 200);
       base64_decode(initial, (unsigned char *)correction);
       mxinfo(correction);
-      mxexit(0);
+      mxexit();
     }
 
   for (aidx = 0; engage_args.size() > aidx; aidx++) {

src/extract/mkvextract.cpp
@@ -132,5 +132,5 @@ main(int argc,
   else
     usage(2);
 
-  mxexit(0);
+  mxexit();
 }

src/info/mkvinfo.cpp
@@ -1761,5 +1761,5 @@ main(int argc,
   else
     console_main();
 
-  mxexit(0);
+  mxexit();
 }

src/merge/mkvmerge.cpp
@@ -1802,18 +1802,18 @@ parse_args(std::vector<std::string> args) {
 
     if ((this_arg == "-l") || (this_arg == "--list-types")) {
       list_file_types();
-      mxexit(0);
+      mxexit();
 
     } else if (this_arg == "--list-languages") {
       list_iso639_languages();
-      mxexit(0);
+      mxexit();
 
     } else if ((this_arg == "-i") || (this_arg == "--identify") || (this_arg == "-I") || (this_arg == "--identify-verbose") || (this_arg == "--identify-for-mmg"))
       mxerror(boost::format(Y("'%1%' can only be used with a file name. No further options are allowed if this option is used.\n")) % this_arg);
 
     else if (this_arg == "--capabilities") {
       print_capabilities();
-      mxexit(0);
+      mxexit();
 
     }
 
@@ -2529,5 +2529,5 @@ main(int argc,
 
   cleanup();
 
-  mxexit(0);
+  mxexit();
 }

src/mkvtoolnix-gui/mkvtoolnix_gui.cpp
@@ -42,5 +42,5 @@ main(int argc,
   mainWindow.reset();
   app.reset();
 
-  mxexit(0);
+  mxexit();
 }

src/propedit/propedit.cpp
@@ -122,7 +122,7 @@ run(options_cptr &options) {
 
   mxinfo(Y("Done.\n"));
 
-  mxexit(0);
+  mxexit();
 }
 
 static
@@ -152,5 +152,5 @@ main(int argc,
 
   run(options);
 
-  mxexit(0);
+  mxexit();
 }

src/propedit/propedit_cli_parser.cpp
@@ -150,7 +150,7 @@ propedit_cli_parser_c::list_property_names() {
   mxinfo(Y("  X:  binary in hex\n"));
   mxinfo(Y("  FP: floating point number\n"));
 
-  mxexit(0);
+  mxexit();
 }
 
 void

src/tools/ac3parser.cpp
@@ -34,13 +34,13 @@ show_help() {
          "\n"
          "  -h, --help             This help text\n"
          "  -V, --version          Print version information\n");
-  mxexit(0);
+  mxexit();
 }
 
 static void
 show_version() {
   mxinfo("ac3parser v" VERSION "\n");
-  mxexit(0);
+  mxexit();
 }
 
 static std::string
@@ -125,5 +125,5 @@ main(int argc,
     mxerror(Y("File not found\n"));
   }
 
-  mxexit(0);
+  mxexit();
 }

src/tools/base64tool.cpp
@@ -122,5 +122,5 @@ main(int argc,
 
   mxinfo(Y("Done.\n"));
 
-  mxexit(0);
+  mxexit();
 }

src/tools/checksum.cpp
@@ -64,13 +64,13 @@ show_help() {
          "\n"
          "  -h, --help             This help text\n"
          "  -V, --version          Print version information\n");
-  mxexit(0);
+  mxexit();
 }
 
 static void
 show_version() {
   mxinfo("checksum v" VERSION "\n");
-  mxexit(0);
+  mxexit();
 }
 
 static cli_options_c
@@ -206,5 +206,5 @@ main(int argc,
     mxerror("File not found\n");
   }
 
-  mxexit(0);
+  mxexit();
 }

src/tools/diracparser.cpp
@@ -150,13 +150,13 @@ show_help() {
            "\n"
            "  -h, --help             This help text\n"
            "  -V, --version          Print version information\n"));
-  mxexit(0);
+  mxexit();
 }
 
 static void
 show_version() {
   mxinfo("diracparser v" VERSION "\n");
-  mxexit(0);
+  mxexit();
 }
 
 static std::string
@@ -232,5 +232,5 @@ main(int argc,
     mxerror(Y("File not found\n"));
   }
 
-  mxexit(0);
+  mxexit();
 }

src/tools/ebml_validator.cpp
@@ -76,13 +76,13 @@ show_help() {
            "\n"
            "  -h, --help             This help text\n"
            "  -V, --version          Print version information\n"));
-  mxexit(0);
+  mxexit();
 }
 
 static void
 show_version() {
   mxinfo("ebml_validator v" VERSION "\n");
-  mxexit(0);
+  mxexit();
 }
 
 static std::string
@@ -368,5 +368,5 @@ main(int argc,
     mxerror(Y("File not found\n"));
   }
 
-  mxexit(0);
+  mxexit();
 }

src/tools/mpls_dump.cpp
@@ -21,13 +21,13 @@ show_help() {
          "\n"
          "  -h, --help             This help text\n"
          "  -V, --version          Print version information\n");
-  mxexit(0);
+  mxexit();
 }
 
 static void
 show_version() {
   mxinfo("mpls_dump v" VERSION "\n");
-  mxexit(0);
+  mxexit();
 }
 
 static std::string
@@ -82,5 +82,5 @@ main(int argc,
     mxerror(Y("File not found\n"));
   }
 
-  mxexit(0);
+  mxexit();
 }

src/tools/vc1parser.cpp
@@ -271,13 +271,13 @@ show_help() {
            "\n"
            "  -h, --help             This help text\n"
            "  -V, --version          Print version information\n"));
-  mxexit(0);
+  mxexit();
 }
 
 static void
 show_version() {
   mxinfo("vc1parser v" VERSION "\n");
-  mxexit(0);
+  mxexit();
 }
 
 static std::string
@@ -359,5 +359,5 @@ main(int argc,
     mxerror(Y("File not found\n"));
   }
 
-  mxexit(0);
+  mxexit();
 }

tests/test-321vc1_without_markers.rb
@@ -1,13 +1,4 @@
 #!/usr/bin/ruby -w
 
-class T_321vc1_without_markers < Test
-  def description
-    "mkvmerge / VC1 without start markers (0x00 0x00 0x01)"
-  end
-
-  def run
-    merge "-A data/mkv/vc1-without-markers.mkv"
-    hash_tmp
-  end
-end
-
+describe "mkvmerge / VC1 without start markers (0x00 0x00 0x01)"
+test_merge "-A data/mkv/vc1-without-markers.mkv", :exit_code => :warning

tests/test-353ac3_from_ts_with_missing_tcs_with_non_zero_first_tc.rb
@@ -2,5 +2,4 @@
 
 # T_353ac3_from_ts_with_missing_tcs_with_non_zero_first_tc
 describe "mkvmerge / MPEG-TS with AC3, TS provides few timecodes, first TC is non-zero"
-
-test_merge "--no-video data/ts/avc-ac3-with-missing-tc-and-non-zero-first-tc.ts"
+test_merge "--no-video data/ts/avc-ac3-with-missing-tc-and-non-zero-first-tc.ts", :exit_code => :warning

tests/test-397mpeg_ts_broken_pes_track_detection.rb
@@ -2,4 +2,4 @@
 
 # T_397mpeg_ts_broken_pes_track_detection
 describe "mkvmerge / MPEG TS broken PES packets, track detection issues"
-test_merge "data/ts/ac3-track-broken-pes-detection.ts"
+test_merge "data/ts/ac3-track-broken-pes-detection.ts", :exit_code => :warning

tests/test-432concatenate_two_ac3_files.rb
@@ -2,4 +2,4 @@
 
 # T_432concatenate_two_ac3_files
 describe "mkvmerge / read from same file twice via concatenation"
-test_merge "'(' data/ac3/misdetected_as_mp2.ac3 data/ac3/misdetected_as_mp2.ac3  ')'"
+test_merge "'(' data/ac3/misdetected_as_mp2.ac3 data/ac3/misdetected_as_mp2.ac3  ')'", :exit_code => :warning

tests/test-433matroska_no_track_uid.rb
@@ -2,4 +2,4 @@
 
 # T_433matroska_no_track_uid
 describe "mkvmerge / Matroska files missing the track UID element"
-test_merge "data/mkv/no-track-uid.mks", :exit => :warning
+test_merge "data/mkv/no-track-uid.mks", :exit_code => :warning

tests/test-449segfaults_assertions.rb
@@ -11,7 +11,7 @@ test_merge "#{dir}/1089-3.mkv"
 
 test_info "#{dir}/1089-1.mkv", :args => "-v -v"
 test "1089-2.mkv" do
-  out = info "#{dir}/1089-2.mkv", :args => "-v -v", :output => :return
+  out = info "#{dir}/1089-2.mkv", :args => "-v -v", :output => :return, :exit_code => :warning
   out.join("\n").gsub(/\(0x.*?\)/, 'xxx').md5
 end
 test_info "#{dir}/1089-3.mkv", :args => "-v -v"

tests/test-451aac_loas_latm_raw.rb
@@ -2,4 +2,4 @@
 
 # T_451aac_loas_latm_raw
 describe "mkvmerge / AAC in raw LOAS/LATM multiplex"
-test_merge "data/aac/aac_loas_latm.aac"
+test_merge "data/aac/aac_loas_latm.aac", :exit_code => :warning