725b3f289ac4a362625e6351df76935b6272ce43
Moritz Bunkus <moritz@bunkus.org>
2015-03-27 12:54:39 +0100

    mkvextract HEVC: refactoring (coding style)

--- src/extract/xtr_hevc.cpp
+++ src/extract/xtr_hevc.cpp
@@ -21,33 +21,32 @@ xtr_hevc_c::create_file(xtr_base_c *master,
                         KaxTrackEntry &track) {
   xtr_base_c::create_file(master, track);
 
-  KaxCodecPrivate *priv = FindChild<KaxCodecPrivate>(&track);
+  auto priv = FindChild<KaxCodecPrivate>(&track);
   if (!priv)
     mxerror(boost::format(Y("Track %1% with the CodecID '%2%' is missing the \"codec private\" element and cannot be extracted.\n")) % m_tid % m_codec_id);
 
-  memory_cptr mpriv = decode_codec_private(priv);
-
+  auto mpriv = decode_codec_private(priv);
   if (mpriv->get_size() < 23)
     mxerror(boost::format(Y("Track %1% CodecPrivate is too small.\n")) % m_tid);
 
-  binary *buf = mpriv->get_buffer();
-
+  auto buf        = mpriv->get_buffer();
   m_nal_size_size = 1 + (buf[21] & 3);
 
   // Parameter sets in this order: vps, sps, pps, sei
-  unsigned int num_parameter_sets = buf[22];
+  auto num_parameter_sets = static_cast<unsigned int>(buf[22]);
+  auto pos                = static_cast<size_t>(23);
 
-  size_t i = 0;
-  size_t pos = 23;
-  while((i < num_parameter_sets) && (mpriv->get_size() > pos)) {
-    pos++; // skip over the nal unit type
+  while (num_parameter_sets && (mpriv->get_size() > (pos + 3))) {
+    auto nal_unit_count  = get_uint16_be(&buf[pos + 1]);
+    pos                 += 3;
 
-    unsigned int nal_unit_count = (buf[pos] << 8) | buf[pos+1];
-    pos += 2;
-
-    for(size_t j = 0; (j < nal_unit_count) && (mpriv->get_size() > pos); j++, i++)
+    while (nal_unit_count && (mpriv->get_size() > pos)) {
       if (!write_nal(buf, pos, mpriv->get_size(), 2))
-        break;
+        return;
+
+      --nal_unit_count;
+      --num_parameter_sets;
+    }
   }
 }
 
