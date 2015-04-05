e89c3f165b616103bf9c5cf026c802d478138eab
Moritz Bunkus <moritz@bunkus.org>
2015-04-05 22:26:30 +0200

    Revert "mmg: reset dialog variable when update dialog is closed"
    This reverts commit 2f82b3a3d5a9a6b308ecb3c883d856ab6adb4412.

--- src/mmg/mmg_dialog.cpp
+++ src/mmg/mmg_dialog.cpp
@@ -1798,7 +1798,6 @@ mmg_dialog::on_update_check_state_changed(wxCommandEvent &evt) {
   } else if (UPDATE_CHECK_DONE_DIALOG_DISMISSED == state) {
     wxMutexLocker locker(m_update_check_mutex);
     m_checking_for_updates = false;
-    m_update_check_dlg     = nullptr;
 
   } else {
     wxConfigBase *cfg = wxConfigBase::Get();
