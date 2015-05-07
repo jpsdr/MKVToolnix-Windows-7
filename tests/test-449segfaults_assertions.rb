c210124b94e109540993ace5a45a660abc484c2b
Moritz Bunkus <moritz@bunkus.org>
2015-05-07 22:23:52 +0200

    tests: update test case due to e0ab8a1
    mkvinfo now exits correctly with 2 if an error happens. The test case
    didn't take that into account.

--- tests/test-449segfaults_assertions.rb
+++ tests/test-449segfaults_assertions.rb
@@ -9,7 +9,7 @@ test_merge "#{dir}/1089-1.mkv", :exit_code => :error
 test_merge "#{dir}/1089-2.mkv", :exit_code => :error
 test_merge "#{dir}/1089-3.mkv"
 
-test_info "#{dir}/1089-1.mkv", :args => "-v -v"
+test_info "#{dir}/1089-1.mkv", :args => "-v -v", :exit_code => :error
 test "1089-2.mkv" do
   out = info "#{dir}/1089-2.mkv", :args => "-v -v", :output => :return, :exit_code => :warning
   out.join("\n").gsub(/\(0x.*?\)/, 'xxx').md5
@@ -22,8 +22,8 @@ test_merge "#{dir}/1096-id:000002,sig:06,src:000000,op:flip2,pos:582.mkv"
 test_merge "#{dir}/1096-id:000003,sig:06,src:000000,op:flip2,pos:606.mkv"
 test_merge "#{dir}/1096-id:000004,sig:06,src:000000,op:flip4,pos:582.mkv"
 
-test_info "#{dir}/1096-id:000000,sig:06,src:000000,op:flip1,pos:0.mkv", :args => "-v -v"
-test_info "#{dir}/1096-id:000001,sig:06,src:000000,op:flip1,pos:0.mkv", :args => "-v -v"
+test_info "#{dir}/1096-id:000000,sig:06,src:000000,op:flip1,pos:0.mkv", :args => "-v -v", :exit_code => :error
+test_info "#{dir}/1096-id:000001,sig:06,src:000000,op:flip1,pos:0.mkv", :args => "-v -v", :exit_code => :error
 test_info "#{dir}/1096-id:000002,sig:06,src:000000,op:flip2,pos:582.mkv", :args => "-v -v"
 test_info "#{dir}/1096-id:000003,sig:06,src:000000,op:flip2,pos:606.mkv", :args => "-v -v"
 test_info "#{dir}/1096-id:000004,sig:06,src:000000,op:flip4,pos:582.mkv", :args => "-v -v"
