jpsdr/MKVToolnix-Windows-7

commit 1e031cbd9184916f7473a63dca8583ea1e8dcb35

GUI: don't add playlist when user aborts select playlist dialog

src/mkvtoolnix-gui/merge/playlist_scanner.cpp
@@ -22,52 +22,62 @@ PlaylistScanner::PlaylistScanner(QWidget *parent)
 {
 }
 
-void
-PlaylistScanner::checkAddingPlaylists(QList<SourceFilePtr> &files) {
+SourceFilePtr
+PlaylistScanner::checkOneFile(SourceFilePtr const &file)
+  const {
+  if (!file->isPlaylist())
+    return file;
+
+  auto info       = QFileInfo{file->m_fileName};
+  auto otherFiles = QDir{info.path()}.entryInfoList(QStringList{QString{"*.%1"}.arg(info.suffix())}, QDir::Files, QDir::Name);
+  if (otherFiles.isEmpty())
+    return file;
+
+  auto doScan = Util::Settings::AlwaysScan == Util::Settings::get().m_scanForPlaylistsPolicy;
+  if (!doScan)
+    doScan = askScanForPlaylists(*file, otherFiles.size());
+
+  if (!doScan)
+    return {};
+
+  auto identifiedFiles = scanForPlaylists(otherFiles);
+  if (identifiedFiles.isEmpty())
+    return file;
+
+  if (1 == identifiedFiles.size())
+    return identifiedFiles[0];
+
+  return SelectPlaylistDialog{m_parent, identifiedFiles}.select();
+}
+
+QList<SourceFilePtr>
+PlaylistScanner::checkAddingPlaylists(QList<SourceFilePtr> const &files)
+  const {
   if (Util::Settings::NeverScan == Util::Settings::get().m_scanForPlaylistsPolicy)
-    return;
+    return files;
+
+  auto actualFiles = QList<SourceFilePtr>{};
 
   for (auto &file : files) {
-    if (!file->isPlaylist())
-      continue;
-
-    auto info       = QFileInfo{file->m_fileName};
-    auto otherFiles = QDir{info.path()}.entryInfoList(QStringList{QString{"*.%1"}.arg(info.suffix())}, QDir::Files, QDir::Name);
-    if (otherFiles.isEmpty())
-      continue;
-
-    auto doScan = Util::Settings::AlwaysScan == Util::Settings::get().m_scanForPlaylistsPolicy;
-    if (!doScan)
-      doScan = askScanForPlaylists(*file, otherFiles.size());
-
-    if (!doScan)
-      continue;
-
-    auto identifiedFiles = scanForPlaylists(otherFiles);
-    if (identifiedFiles.isEmpty())
-      continue;
-
-    else if (1 == identifiedFiles.size())
-      file = identifiedFiles[0];
-
-    else {
-      auto selectedFile = SelectPlaylistDialog{m_parent, identifiedFiles}.select();
-      if (selectedFile)
-        file = selectedFile;
-    }
+    auto actualFile = checkOneFile(file);
+    if (actualFile)
+      actualFiles << actualFile;
   }
+
+  return actualFiles;
 }
 
 bool
 PlaylistScanner::askScanForPlaylists(SourceFile const &file,
-                                     unsigned int numOtherFiles) {
-
+                                     unsigned int numOtherFiles)
+  const {
   AskScanForPlaylistsDialog dialog{m_parent};
   return dialog.ask(file, numOtherFiles);
 }
 
 QList<SourceFilePtr>
-PlaylistScanner::scanForPlaylists(QFileInfoList const &otherFiles) {
+PlaylistScanner::scanForPlaylists(QFileInfoList const &otherFiles)
+  const {
   QProgressDialog progress{ QY("Scanning directory"), QY("Cancel"), 0, otherFiles.size(), m_parent };
   progress.setWindowModality(Qt::ApplicationModal);
 

src/mkvtoolnix-gui/merge/input.cpp
@@ -763,7 +763,7 @@ Tab::addOrAppendFiles(bool append,
   }
 
   if (!append)
-    PlaylistScanner{this}.checkAddingPlaylists(identifiedFiles);
+    identifiedFiles = PlaylistScanner{this}.checkAddingPlaylists(identifiedFiles);
 
   if (identifiedFiles.isEmpty())
     return;

src/mkvtoolnix-gui/merge/playlist_scanner.h
@@ -18,11 +18,12 @@ protected:
 public:
   explicit PlaylistScanner(QWidget *parent);
 
-  void checkAddingPlaylists(QList<SourceFilePtr> &files);
+  QList<SourceFilePtr> checkAddingPlaylists(QList<SourceFilePtr> const &files) const;
 
 protected:
-  bool askScanForPlaylists(SourceFile const &file, unsigned int numOtherFiles);
-  QList<SourceFilePtr> scanForPlaylists(QFileInfoList const &otherFiles);
+  bool askScanForPlaylists(SourceFile const &file, unsigned int numOtherFiles) const;
+  QList<SourceFilePtr> scanForPlaylists(QFileInfoList const &otherFiles) const;
+  SourceFilePtr checkOneFile(SourceFilePtr const &file) const;
 };
 
 }}}

commit d441c0cc0cee8431882a4e997903078920827968

GUI: fix playlist adding part 2
Required fix for 1e031cb which broke the »don't scan, just add«
functionality of the dialog asking whether or not to scan for other
playlists.

src/mkvtoolnix-gui/merge/playlist_scanner.cpp
@@ -38,7 +38,7 @@ PlaylistScanner::checkOneFile(SourceFilePtr const &file)
     doScan = askScanForPlaylists(*file, otherFiles.size());
 
   if (!doScan)
-    return {};
+    return file;
 
   auto identifiedFiles = scanForPlaylists(otherFiles);
   if (identifiedFiles.isEmpty())