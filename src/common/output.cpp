jpsdr/MKVToolnix-Windows-7

commit 71b205a97598dc708aaefe2e031b2798b848cb7d

GUI: fix warning/error message recognition for non-English UI languages

src/common/output.cpp
@@ -15,6 +15,7 @@
 
 #include <sstream>
 
+#include "common/command_line.h"
 #include "common/ebml.h"
 #include "common/endian.h"
 #include "common/locale.h"
@@ -75,11 +76,12 @@ mxmsg(unsigned int level,
   if (level == MXMSG_ERROR) {
     if (s_saw_cr_after_nl)
       g_mm_stdio->puts("\n");
-    if (!balg::starts_with(message, Y("Error:")))
-      g_mm_stdio->puts(Y("Error: "));
+    if (balg::starts_with(message, Y("Error:")))
+      message.erase(0, std::string{Y("Error:")}.length());
+    g_mm_stdio->puts(g_gui_mode ? "#GUI#error " : (boost::format("%1 ") % Y("Error:")).str());
 
   } else if (level == MXMSG_WARNING)
-    g_mm_stdio->puts(Y("Warning: "));
+    g_mm_stdio->puts(g_gui_mode ? "#GUI#warning " : (boost::format("%1 ") % Y("Warning:")).str());
 
   else if (level == MXMSG_DEBUG)
     g_mm_stdio->puts(Y("Debug> "));

src/mkvtoolnix-gui/jobs/job.cpp
@@ -83,10 +83,10 @@ Job::addLineToInternalLogs(QString const &line,
                 :                       m_errors;
 
   auto prefix   = InfoLine    == type ? Q("")
-                : WarningLine == type ? QY("Warning: ")
-                :                       QY("Error: ");
+                : WarningLine == type ? Q("%1 ").arg(QY("Warning:"))
+                :                       Q("%1 ").arg(QY("Error:"));
 
-  m_fullOutput << Q("%1%2\n").arg(prefix).arg(line);
+  m_fullOutput << Q("%1%2").arg(prefix).arg(line);
   storage      << line;
 
   if ((WarningLine == type) || (ErrorLine == type))

src/mkvtoolnix-gui/jobs/mux_job.cpp
@@ -78,15 +78,14 @@ MuxJob::processLine(QString const &rawLine) {
 
   line.replace(QRegularExpression{"[\r\n]+"}, "");
 
-  // TODO: MuxJob::processLine
-  if (line.startsWith("Warning:")) {
-    line.replace(QRegularExpression{"^Warning: *"}, "");
+  if (line.startsWith("#GUI#warning ")) {
+    line.replace(QRegularExpression{"^#GUI#warning *"}, "");
     emit lineRead(line, WarningLine);
     return;
   }
 
-  if (line.startsWith("Error:")) {
-    line.replace(QRegularExpression{"^Error: *"}, "");
+  if (line.startsWith("#GUI#error ")) {
+    line.replace(QRegularExpression{"^#GUI#error *"}, "");
     emit lineRead(line, ErrorLine);
     return;
   }

src/mkvtoolnix-gui/watch_jobs/tab.cpp
@@ -177,8 +177,12 @@ Tab::onLineRead(QString const &line,
                 : Jobs::Job::WarningLine == type ? ui->warnings
                 :                                  ui->errors;
 
+  auto prefix   = Jobs::Job::InfoLine    == type ? Q("")
+                : Jobs::Job::WarningLine == type ? Q("%1 ").arg(QY("Warning:"))
+                :                                  Q("%1 ").arg(QY("Error:"));
+
+  m_fullOutput << Q("%1%2").arg(prefix).arg(line);
   storage->appendPlainText(line);
-  m_fullOutput << line;
 
   if (mtx::included_in(type, Jobs::Job::WarningLine, Jobs::Job::ErrorLine))
     ui->acknowledgeWarningsAndErrorsButton->setEnabled(true);

src/mmg/jobs.cpp
@@ -270,7 +270,14 @@ job_run_dialog::process_input() {
           if ((value >= 0) && (value <= 100))
             set_progress_value(value);
         }
-      } else if (wx_line.Length() > 0)
+
+      } else if (wx_line.Find(wxT("#GUI#warning")) == 0)
+        *jobs[jobs_to_start[current_job]].log += Z("Warning:") + wxT(" ") + wx_line.Mid(11) + wxT("\n");
+
+      else if (wx_line.Find(wxT("#GUI#error")) == 0)
+        *jobs[jobs_to_start[current_job]].log += Z("Error:") + wxT(" ") + wx_line.Mid(13) + wxT("\n");
+
+      else if (wx_line.Length() > 0)
         *jobs[jobs_to_start[current_job]].log += wx_line + wxT("\n");
       line = "";
     } else if ((unsigned char)c != 0xff)

src/mmg/mux_dialog.cpp
@@ -277,11 +277,11 @@ mux_dialog::on_output_available(wxCommandEvent &evt) {
   if (line.Right(1) != wxT("\r"))
     log += wxT("\n");
 
-  if (line.Find(Z("Warning:")) == 0)
-    tc_warnings->AppendText(line + wxT("\n"));
+  if (line.Find(wxT("#GUI#warning")) == 0)
+    tc_warnings->AppendText(Z("Warning:") + wxT(" ") + line.Mid(13) + wxT("\n"));
 
-  else if (line.Find(Z("Error:")) == 0)
-    tc_errors->AppendText(line + wxT("\n"));
+  else if (line.Find(wxT("#GUI#error")) == 0)
+    tc_errors->AppendText(Z("Error:") + wxT(" ") + line.Mid(11) + wxT("\n"));
 
   else if (line.Find(wxT("#GUI#begin_scanning_playlists")) == 0)
     m_scanning_playlists = true;

commit 0754b80bc26406810018d73cbb0744f64d37ac44

all: fix wrong Boost format string introduced in 71b205a

src/common/output.cpp
@@ -78,10 +78,10 @@ mxmsg(unsigned int level,
       g_mm_stdio->puts("\n");
     if (balg::starts_with(message, Y("Error:")))
       message.erase(0, std::string{Y("Error:")}.length());
-    g_mm_stdio->puts(g_gui_mode ? "#GUI#error " : (boost::format("%1 ") % Y("Error:")).str());
+    g_mm_stdio->puts(g_gui_mode ? "#GUI#error " : (boost::format("%1% ") % Y("Error:")).str());
 
   } else if (level == MXMSG_WARNING)
-    g_mm_stdio->puts(g_gui_mode ? "#GUI#warning " : (boost::format("%1 ") % Y("Warning:")).str());
+    g_mm_stdio->puts(g_gui_mode ? "#GUI#warning " : (boost::format("%1% ") % Y("Warning:")).str());
 
   else if (level == MXMSG_DEBUG)
     g_mm_stdio->puts(Y("Debug> "));