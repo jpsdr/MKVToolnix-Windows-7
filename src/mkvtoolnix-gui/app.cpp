jpsdr/MKVToolnix-Windows-7

commit 0c6246dc122d29adcded34cb4abd5fc73557f7fb

GUI: Windows: store settings in INI files in user's data folder if installed
Includes migrating existing settings from the registry.

The background is that writing large amounts of data (as is done if the
queue's big) is kind of slow. This is in preparation for more changes,
e.g. storing each queue job in its own file instead of all queue jobs in
a single INI file.

src/mkvtoolnix-gui/app.cpp
@@ -48,9 +48,10 @@ App::App(int &argc,
 
 #ifdef SYS_WINDOWS
   QApplication::setStyle(Q("windowsvista"));
+  Util::Settings::migrateFromRegistry();
 #endif
 
-  Util::Settings::get().load();
+  Util::Settings::get();        // Ensure that the settings have been loaded
 
   setupInstanceCommunicator();
 

src/mkvtoolnix-gui/util/settings.cpp
@@ -3,9 +3,9 @@
 #include <QDebug>
 #include <QSettings>
 #include <QSplitter>
+#include <QStandardPaths>
 
 #include "common/extern_data.h"
-#include "common/fs_sys_helpers.h"
 #include "common/iso639.h"
 #include "common/qt.h"
 #include "common/version.h"
@@ -32,13 +32,49 @@ Settings::get() {
   return s_settings;
 }
 
+#if defined(SYS_WINDOWS)
+QString
+Settings::iniFileLocation() {
+  auto path = App::isInstalled() ? QStandardPaths::writableLocation(QStandardPaths::ConfigLocation) : App::applicationDirPath();
+  return Q("%1/mkvtoolnix-gui.ini").arg(path);
+}
+
+void
+Settings::migrateFromRegistry() {
+  // Migration of settings from the Windows registry into an .ini file
+  // due to performance issues.
+
+  // If this is the portable version the no settings have to be migrated.
+  if (!App::isInstalled())
+    return;
+
+  // Don't do anything if such a file exists already.
+  auto targetFileName = iniFileLocation();
+  if (QFileInfo{targetFileName}.exists())
+    return;
+
+  // Copy all settings.
+  QSettings target{targetFileName, QSettings::IniFormat};
+  QSettings source{};
+
+  for (auto const &key : source.allKeys())
+    target.setValue(key, source.value(key));
+
+  // Ensure the new file is written and remove the keys from the
+  // registry.
+  target.sync();
+  source.clear();
+  source.sync();
+}
+#endif
+
 std::unique_ptr<QSettings>
 Settings::registry() {
 #if defined(SYS_WINDOWS)
-  if (!App::isInstalled())
-    return std::make_unique<QSettings>(Q((mtx::sys::get_installation_path() / "mkvtoolnix-gui.ini").string()), QSettings::IniFormat);
-#endif
+  return std::make_unique<QSettings>(iniFileLocation(), QSettings::IniFormat);
+#else
   return std::make_unique<QSettings>();
+#endif
 }
 
 void
@@ -246,7 +282,7 @@ Settings::exeWithPath(QString const &exe) {
   if (path.is_absolute())
     return exe;
 
-  auto installPath   = mtx::sys::get_installation_path();
+  auto installPath   = bfs::path{ to_utf8(App::applicationDirPath()) };
   auto potentialExes = QList<bfs::path>{} << (installPath / path) << (installPath / ".." / path);
 
 #if defined(SYS_WINDOWS)

src/mkvtoolnix-gui/util/settings.h
@@ -110,6 +110,11 @@ public:
   static std::unique_ptr<QSettings> registry();
 
   static QString exeWithPath(QString const &exe);
+
+#if defined(SYS_WINDOWS)
+  static void migrateFromRegistry();
+  static QString iniFileLocation();
+#endif
 };
 
 // extern Settings g_settings;

commit 838bec431056dc972c904c6543ea2a3ed9621860

GUI: fix loading preferences (broken in 0c6246d)

src/mkvtoolnix-gui/app.cpp
@@ -51,7 +51,7 @@ App::App(int &argc,
   Util::Settings::migrateFromRegistry();
 #endif
 
-  Util::Settings::get();        // Ensure that the settings have been loaded
+  Util::Settings::get().load();
 
   setupInstanceCommunicator();
 

src/mkvtoolnix-gui/util/settings.cpp
@@ -16,9 +16,7 @@ namespace mtx { namespace gui { namespace Util {
 
 Settings Settings::s_settings;
 
-Settings::Settings()
-{
-  load();
+Settings::Settings() {
 }
 
 void