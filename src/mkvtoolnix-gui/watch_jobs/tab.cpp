jpsdr/MKVToolnix-Windows-7

commit ad40b749cf776d4d524a81d0d5eaba8c1ca95b3c

GUI: only access fields in Job directly via accessors
This allows for checking the modification state.

src/mkvtoolnix-gui/watch_jobs/tab.cpp
@@ -121,23 +121,23 @@ Tab::onStatusChanged(uint64_t id,
     return;
   }
 
-  QMutexLocker locker{&job->m_mutex};
-  m_currentJobStatus = job->m_status;
-  m_id               = job->m_id;
+  job->action([&]() {
+    m_currentJobStatus = job->status();
 
-  ui->abortButton->setEnabled(Jobs::Job::Running == m_currentJobStatus);
-  m_saveOutputAction->setEnabled(true);
-  ui->status->setText(Jobs::Job::displayableStatus(m_currentJobStatus));
-  MainWindow::watchJobTool()->enableMenuActions();
+    ui->abortButton->setEnabled(Jobs::Job::Running == m_currentJobStatus);
+    m_saveOutputAction->setEnabled(true);
+    ui->status->setText(Jobs::Job::displayableStatus(m_currentJobStatus));
+    MainWindow::watchJobTool()->enableMenuActions();
 
-  // Check for the signalled status, not the current one, in order to
-  // detect a change from »not running« to »running« only once, no
-  // matter which order the signals arrive in.
-  if (Jobs::Job::Running == status)
-    setInitialDisplay(*job);
+    // Check for the signalled status, not the current one, in order to
+    // detect a change from »not running« to »running« only once, no
+    // matter which order the signals arrive in.
+    if (Jobs::Job::Running == status)
+      setInitialDisplay(*job);
 
-  else if (mtx::included_in(m_currentJobStatus, Jobs::Job::DoneOk, Jobs::Job::DoneWarnings, Jobs::Job::Failed, Jobs::Job::Aborted))
-    ui->finishedAt->setText(Util::displayableDate(job->m_dateFinished));
+    else if (mtx::included_in(m_currentJobStatus, Jobs::Job::DoneOk, Jobs::Job::DoneWarnings, Jobs::Job::Failed, Jobs::Job::Aborted))
+      ui->finishedAt->setText(Util::displayableDate(job->dateFinished()));
+  });
 
   updateRemainingTime();
 }
@@ -217,36 +217,39 @@ Tab::onLineRead(QString const &line,
 
 void
 Tab::setInitialDisplay(Jobs::Job const &job) {
+  auto dateStarted = Util::displayableDate(job.dateStarted());
+  auto description = job.description();
+
   if (isCurrentJobTab() && Util::Settings::get().m_showOutputOfAllJobs) {
-    auto outputOfJobLine = QY("--- Output of job '%1' started on %2 ---").arg(job.m_description).arg(Util::displayableDate(job.m_dateStarted));
-    m_fullOutput << outputOfJobLine << job.m_fullOutput;
+    auto outputOfJobLine = QY("--- Output of job '%1' started on %2 ---").arg(description).arg(dateStarted);
+    m_fullOutput << outputOfJobLine << job.fullOutput();
 
     ui->output  ->appendPlainText(outputOfJobLine);
-    ui->warnings->appendPlainText(QY("--- Warnings emitted by job '%1' started on %2 ---").arg(job.m_description).arg(Util::displayableDate(job.m_dateStarted)));
-    ui->errors  ->appendPlainText(QY("--- Errors emitted by job '%1' started on %2 ---").arg(job.m_description).arg(Util::displayableDate(job.m_dateStarted)));
+    ui->warnings->appendPlainText(QY("--- Warnings emitted by job '%1' started on %2 ---").arg(description).arg(dateStarted));
+    ui->errors  ->appendPlainText(QY("--- Errors emitted by job '%1' started on %2 ---").arg(description).arg(dateStarted));
 
   } else {
-    m_fullOutput = job.m_fullOutput;
+    m_fullOutput = job.fullOutput();
 
-    ui->output  ->setPlainText(!job.m_output.isEmpty()   ? Q("%1\n").arg(job.m_output.join("\n"))   : Q(""));
-    ui->warnings->setPlainText(!job.m_warnings.isEmpty() ? Q("%1\n").arg(job.m_warnings.join("\n")) : Q(""));
-    ui->errors  ->setPlainText(!job.m_errors.isEmpty()   ? Q("%1\n").arg(job.m_errors.join("\n"))   : Q(""));
+    ui->output  ->setPlainText(!job.output().isEmpty()   ? Q("%1\n").arg(job.output().join("\n"))   : Q(""));
+    ui->warnings->setPlainText(!job.warnings().isEmpty() ? Q("%1\n").arg(job.warnings().join("\n")) : Q(""));
+    ui->errors  ->setPlainText(!job.errors().isEmpty()   ? Q("%1\n").arg(job.errors().join("\n"))   : Q(""));
   }
 
-  m_currentJobStatus    = job.m_status;
-  m_currentJobProgress  = job.m_progress;
-  m_currentJobStartTime = job.m_dateStarted;
+  m_currentJobStatus    = job.status();
+  m_currentJobProgress  = job.progress();
+  m_currentJobStartTime = job.dateStarted();
   m_queueProgress       = MainWindow::watchCurrentJobTab()->m_queueProgress;
 
-  ui->description->setText(job.m_description);
-  ui->status->setText(Jobs::Job::displayableStatus(job.m_status));
-  ui->progressBar->setValue(job.m_progress);
+  ui->description->setText(description);
+  ui->status->setText(Jobs::Job::displayableStatus(job.status()));
+  ui->progressBar->setValue(job.progress());
 
-  ui->startedAt ->setText(job.m_dateStarted .isValid() ? Util::displayableDate(job.m_dateStarted)  : QY("not started yet"));
-  ui->finishedAt->setText(job.m_dateFinished.isValid() ? Util::displayableDate(job.m_dateFinished) : QY("not finished yet"));
+  ui->startedAt ->setText(job.dateStarted() .isValid() ? Util::displayableDate(job.dateStarted())  : QY("not started yet"));
+  ui->finishedAt->setText(job.dateFinished().isValid() ? Util::displayableDate(job.dateFinished()) : QY("not finished yet"));
 
-  ui->abortButton->setEnabled(Jobs::Job::Running == job.m_status);
-  m_saveOutputAction->setEnabled(!mtx::included_in(job.m_status, Jobs::Job::PendingManual, Jobs::Job::PendingAuto, Jobs::Job::Disabled));
+  ui->abortButton->setEnabled(Jobs::Job::Running == job.status());
+  m_saveOutputAction->setEnabled(!mtx::included_in(job.status(), Jobs::Job::PendingManual, Jobs::Job::PendingAuto, Jobs::Job::Disabled));
 
   ui->acknowledgeWarningsAndErrorsButton->setEnabled(job.numUnacknowledgedWarnings() || job.numUnacknowledgedErrors());
 

src/mkvtoolnix-gui/merge/tab.cpp
@@ -285,11 +285,12 @@ Tab::addToJobQueue(bool startNow) {
   if (!isReadyForMerging())
     return;
 
-  auto &cfg          = Util::Settings::get();
-  auto newConfig     = std::make_shared<MuxConfig>(m_config);
-  auto job           = std::make_shared<Jobs::MuxJob>(startNow ? Jobs::Job::PendingAuto : Jobs::Job::PendingManual, newConfig);
-  job->m_dateAdded   = QDateTime::currentDateTime();
-  job->m_description = job->displayableDescription();
+  auto &cfg      = Util::Settings::get();
+  auto newConfig = std::make_shared<MuxConfig>(m_config);
+  auto job       = std::make_shared<Jobs::MuxJob>(startNow ? Jobs::Job::PendingAuto : Jobs::Job::PendingManual, newConfig);
+
+  job->setDateAdded(QDateTime::currentDateTime());
+  job->setDescription(job->displayableDescription());
 
   if (!startNow) {
     if (!cfg.m_useDefaultJobDescription) {
@@ -297,12 +298,12 @@ Tab::addToJobQueue(bool startNow) {
 
       while (newDescription.isEmpty()) {
         bool ok = false;
-        newDescription = QInputDialog::getText(this, QY("Enter job description"), QY("Please enter the new job's description."), QLineEdit::Normal, job->m_description, &ok);
+        newDescription = QInputDialog::getText(this, QY("Enter job description"), QY("Please enter the new job's description."), QLineEdit::Normal, job->description(), &ok);
         if (!ok)
           return;
       }
 
-      job->m_description = newDescription;
+      job->setDescription(newDescription);
     }
 
     MainWindow::get()->showIconMovingToTool(Q("task-delegate.png"), *MainWindow::jobTool());

src/mkvtoolnix-gui/jobs/job.cpp
@@ -23,12 +23,115 @@ Job::Job(Status status)
   , m_warningsAcknowledged{}
   , m_errorsAcknowledged{}
   , m_quitAfterFinished{}
+  , m_modified{true}
   , m_mutex{QMutex::Recursive}
 {
   connect(this, &Job::lineRead, this, &Job::addLineToInternalLogs);
 }
 
 Job::~Job() {
+}
+
+uint64_t
+Job::id()
+  const {
+  return m_id;
+}
+
+QUuid
+Job::uuid()
+  const {
+  return m_uuid;
+}
+
+Job::Status
+Job::status()
+  const {
+  return m_status;
+}
+
+QString
+Job::description()
+  const {
+  return m_description;
+}
+
+unsigned int
+Job::progress()
+  const {
+  return m_progress;
+}
+
+QStringList const &
+Job::output()
+  const {
+  return m_output;
+}
+
+QStringList const &
+Job::warnings()
+  const {
+  return m_warnings;
+}
+
+QStringList const &
+Job::errors()
+  const {
+  return m_errors;
+}
+
+QStringList const &
+Job::fullOutput()
+  const {
+  return m_fullOutput;
+}
+
+QDateTime
+Job::dateAdded()
+  const {
+  return m_dateAdded;
+}
+
+QDateTime
+Job::dateStarted()
+  const {
+  return m_dateStarted;
+}
+
+QDateTime
+Job::dateFinished()
+  const {
+  return m_dateFinished;
+}
+
+bool
+Job::isModified()
+  const {
+  return m_modified;
+}
+
+void
+Job::setDescription(QString const &pDescription) {
+  m_description = pDescription;
+  m_modified    = true;
+}
+
+void
+Job::setDateAdded(QDateTime const &pDateAdded) {
+  m_dateAdded = pDateAdded;
+  m_modified  = true;
+}
+
+void
+Job::setDateFinished(QDateTime const &pDateFinished) {
+  m_dateFinished = pDateFinished;
+  m_modified     = true;
+}
+
+void
+Job::setQuitAfterFinished(bool pQuitAfterFinished) {
+  m_quitAfterFinished = pQuitAfterFinished;
+  m_modified          = true;
 }
 
 void
@@ -45,7 +148,8 @@ Job::setStatus(Status status) {
   if (status == m_status)
     return;
 
-  m_status = status;
+  m_status   = status;
+  m_modified = true;
 
   if (Running == status) {
     m_dateStarted = QDateTime::currentDateTime();
@@ -76,6 +180,7 @@ Job::setProgress(unsigned int progress) {
     return;
 
   m_progress = progress;
+  m_modified = true;
   emit progressChanged(m_id, m_progress);
 }
 
@@ -108,6 +213,8 @@ Job::addLineToInternalLogs(QString const &line,
 
   m_fullOutput << Q("%1%2").arg(prefix).arg(line);
   storage      << line;
+
+  m_modified    = true;
 
   if ((WarningLine == type) || (ErrorLine == type))
     updateUnacknowledgedWarningsAndErrors();
@@ -169,9 +276,7 @@ Job::saveQueueFile() {
 }
 
 void
-Job::saveJob(Util::ConfigFile &settings)
-  const {
-
+Job::saveJob(Util::ConfigFile &settings) {
   settings.setValue("uuid",                 m_uuid);
   settings.setValue("status",               static_cast<unsigned int>(m_status));
   settings.setValue("description",          m_description);
@@ -188,10 +293,13 @@ Job::saveJob(Util::ConfigFile &settings)
   settings.setValue("dateFinished",         m_dateFinished);
 
   saveJobInternal(settings);
+
+  m_modified = false;
 }
 
 void
 Job::loadJobBasis(Util::ConfigFile &settings) {
+  m_modified             = true;
   m_uuid                 = settings.value("uuid").toUuid();
   m_status               = static_cast<Status>(settings.value("status", static_cast<unsigned int>(PendingManual)).toUInt());
   m_description          = settings.value("description").toString();
@@ -238,12 +346,14 @@ Job::loadJob(Util::ConfigFile &settings) {
 void
 Job::acknowledgeWarnings() {
   m_warningsAcknowledged = m_warnings.count();
+  m_modified             = true;
   updateUnacknowledgedWarningsAndErrors();
 }
 
 void
 Job::acknowledgeErrors() {
   m_errorsAcknowledged = m_errors.count();
+  m_modified           = true;
   updateUnacknowledgedWarningsAndErrors();
 }
 

src/mkvtoolnix-gui/jobs/job.h
@@ -46,7 +46,7 @@ public:
 private:
   static uint64_t ms_next_id;
 
-public:
+protected:
   QUuid m_uuid;
   uint64_t m_id;
   Status m_status;
@@ -54,13 +54,35 @@ public:
   QStringList m_output, m_warnings, m_errors, m_fullOutput;
   unsigned int m_progress, m_exitCode, m_warningsAcknowledged, m_errorsAcknowledged;
   QDateTime m_dateAdded, m_dateStarted, m_dateFinished;
-  bool m_quitAfterFinished;
+  bool m_quitAfterFinished, m_modified;
 
   QMutex m_mutex;
 
 public:
   Job(Status status = PendingManual);
   virtual ~Job();
+
+  bool isModified() const;
+
+  uint64_t id() const;
+  QUuid uuid() const;
+  Status status() const;
+  QString description() const;
+  unsigned int progress() const;
+
+  QStringList const &output() const;
+  QStringList const &warnings() const;
+  QStringList const &errors() const;
+  QStringList const &fullOutput() const;
+
+  QDateTime dateAdded() const;
+  QDateTime dateStarted() const;
+  QDateTime dateFinished() const;
+
+  void setDescription(QString const &pDescription);
+  void setDateAdded(QDateTime const &pDateAdded);
+  void setDateFinished(QDateTime const &pDateFinished);
+  void setQuitAfterFinished(bool pQuitAfterFinished);
 
   void action(std::function<void()> code);
   bool isToBeProcessed() const;
@@ -77,7 +99,7 @@ public:
   QString queueFileName() const;
   void removeQueueFile() const;
   void saveQueueFile();
-  void saveJob(Util::ConfigFile &settings) const;
+  void saveJob(Util::ConfigFile &settings);
 
   void acknowledgeWarnings();
   void acknowledgeErrors();

src/mkvtoolnix-gui/jobs/model.cpp
@@ -101,7 +101,7 @@ Model::hasRunningJobs() {
   QMutexLocker locked{&m_mutex};
 
   for (auto const &job : m_jobsById)
-    if (Job::Running == job->m_status)
+    if (Job::Running == job->status())
       return true;
 
   return false;
@@ -117,13 +117,13 @@ void
 Model::setRowText(QList<QStandardItem *> const &items,
                   Job const &job)
   const {
-  items.at(StatusColumn)      ->setText(Job::displayableStatus(job.m_status));
-  items.at(DescriptionColumn) ->setText(job.m_description);
+  items.at(StatusColumn)      ->setText(Job::displayableStatus(job.status()));
+  items.at(DescriptionColumn) ->setText(job.description());
   items.at(TypeColumn)        ->setText(job.displayableType());
-  items.at(ProgressColumn)    ->setText(to_qs(boost::format("%1%%%") % job.m_progress));
-  items.at(DateAddedColumn)   ->setText(Util::displayableDate(job.m_dateAdded));
-  items.at(DateStartedColumn) ->setText(Util::displayableDate(job.m_dateStarted));
-  items.at(DateFinishedColumn)->setText(Util::displayableDate(job.m_dateFinished));
+  items.at(ProgressColumn)    ->setText(to_qs(boost::format("%1%%%") % job.progress()));
+  items.at(DateAddedColumn)   ->setText(Util::displayableDate(job.dateAdded()));
+  items.at(DateStartedColumn) ->setText(Util::displayableDate(job.dateStarted()));
+  items.at(DateFinishedColumn)->setText(Util::displayableDate(job.dateFinished()));
 
   items[DescriptionColumn ]->setTextAlignment(Qt::AlignLeft  | Qt::AlignVCenter);
   items[ProgressColumn    ]->setTextAlignment(Qt::AlignRight | Qt::AlignVCenter);
@@ -155,7 +155,7 @@ Model::createRow(Job const &job)
     items << new QStandardItem{};
   setRowText(items, job);
 
-  items[0]->setData(QVariant::fromValue(job.m_id), Util::JobIdRole);
+  items[0]->setData(QVariant::fromValue(job.id()), Util::JobIdRole);
 
   return items;
 }
@@ -198,7 +198,7 @@ Model::removeJobsIf(std::function<bool(Job const &)> predicate) {
 
     if (predicate(*job)) {
       job->removeQueueFile();
-      m_jobsById.remove(job->m_id);
+      m_jobsById.remove(job->id());
       toBeRemoved[job] = true;
       removeRow(row - 1);
     }
@@ -217,7 +217,7 @@ void
 Model::add(JobPtr const &job) {
   QMutexLocker locked{&m_mutex};
 
-  m_jobsById[job->m_id] = job;
+  m_jobsById[job->id()] = job;
 
   updateJobStats();
   updateNumUnacknowledgedWarningsOrErrors();
@@ -248,7 +248,7 @@ Model::onStatusChanged(uint64_t id) {
     return;
 
   auto &job       = *m_jobsById[id];
-  auto status     = job.m_status;
+  auto status     = job.status();
   auto numBefore  = m_toBeProcessed.count();
 
   if (job.isToBeProcessed())
@@ -258,11 +258,11 @@ Model::onStatusChanged(uint64_t id) {
     updateProgress();
 
   if (included_in(status, Job::PendingManual, Job::PendingAuto, Job::Running))
-    job.m_dateFinished = QDateTime{};
+    job.setDateFinished(QDateTime{});
 
-  item(row, StatusColumn)->setText(Job::displayableStatus(job.m_status));
-  item(row, DateStartedColumn)->setText(Util::displayableDate(job.m_dateStarted));
-  item(row, DateFinishedColumn)->setText(Util::displayableDate(job.m_dateFinished));
+  item(row, StatusColumn)->setText(Job::displayableStatus(job.status()));
+  item(row, DateStartedColumn)->setText(Util::displayableDate(job.dateStarted()));
+  item(row, DateFinishedColumn)->setText(Util::displayableDate(job.dateFinished()));
 
   if ((Job::Running == status) && !m_running) {
     m_running        = true;
@@ -280,7 +280,7 @@ void
 Model::removeScheduledJobs() {
   QMutexLocker locked{&m_mutex};
 
-  removeJobsIf([this](Job const &job) { return m_toBeRemoved[job.m_id]; });
+  removeJobsIf([this](Job const &job) { return m_toBeRemoved[job.id()]; });
   m_toBeRemoved.clear();
 }
 
@@ -363,9 +363,9 @@ Model::startNextAutoJob() {
   for (auto row = 0, numRows = rowCount(); row < numRows; ++row) {
     auto job = m_jobsById[idFromRow(row)].get();
 
-    if (Job::Running == job->m_status)
+    if (Job::Running == job->status())
       return;
-    if (!toStart && (Job::PendingAuto == job->m_status))
+    if (!toStart && (Job::PendingAuto == job->status()))
       toStart = job;
   }
 
@@ -416,9 +416,9 @@ Model::updateProgress() {
   auto runningProgress  = 0;
 
   for (auto const &job : m_toBeProcessed)
-    if (Job::Running == job->m_status) {
+    if (Job::Running == job->status()) {
       ++numRunning;
-      runningProgress += job->m_progress;
+      runningProgress += job->progress();
 
     } else if (!job->isToBeProcessed() && m_toBeProcessed.contains(job))
       ++numDone;
@@ -438,10 +438,10 @@ Model::updateJobStats() {
   auto numOther         = 0;
 
   for (auto const &job : m_jobsById)
-    if (mtx::included_in(job->m_status, Job::PendingAuto, Job::Running))
+    if (mtx::included_in(job->status(), Job::PendingAuto, Job::Running))
       ++numPendingAuto;
 
-    else if (Job::PendingManual == job->m_status)
+    else if (Job::PendingManual == job->status())
       ++numPendingManual;
 
     else
@@ -472,7 +472,7 @@ Model::convertJobQueueToSeparateIniFiles() {
         continue;
 
       job->saveQueueFile();
-      order << job->m_uuid.toString();
+      order << job->uuid().toString();
 
     } catch (Merge::InvalidSettingsX &) {
     }
@@ -499,7 +499,7 @@ Model::saveJobs() {
     auto &job = m_jobsById[idFromRow(row)];
 
     job->saveQueueFile();
-    order << job->m_uuid.toString();
+    order << job->uuid().toString();
   }
 
   auto reg = Util::Settings::registry();
@@ -537,7 +537,7 @@ Model::loadJobs() {
 
       loadedJobs << job;
 
-      auto uuid = job->m_uuid.toString();
+      auto uuid = job->uuid().toString();
       if (!orderByUuid.contains(uuid))
         orderByUuid[uuid] = orderIdx++;
 
@@ -545,7 +545,7 @@ Model::loadJobs() {
     }
   }
 
-  mtx::sort::by(loadedJobs.begin(), loadedJobs.end(), [&orderByUuid](JobPtr const &job) { return orderByUuid[ job->m_uuid.toString() ]; });
+  mtx::sort::by(loadedJobs.begin(), loadedJobs.end(), [&orderByUuid](JobPtr const &job) { return orderByUuid[ job->uuid().toString() ]; });
 
   for (auto const &job : loadedJobs)
     add(job);

src/mkvtoolnix-gui/jobs/tool.cpp
@@ -116,7 +116,7 @@ Tool::onJobQueueMenu() {
   auto hasPendingManual = false;
 
   m_model->withAllJobs([&hasPendingManual](Job &job) {
-    if (Job::PendingManual == job.m_status)
+    if (Job::PendingManual == job.status())
       hasPendingManual = true;
   });
 
@@ -170,7 +170,7 @@ Tool::onStartAllPending() {
   auto startedSomething = false;
 
   m_model->withAllJobs([&startedSomething](Job &job) {
-    if (Job::PendingManual == job.m_status) {
+    if (Job::PendingManual == job.status()) {
       job.setPendingAuto();
       startedSomething = true;
     }
@@ -187,12 +187,12 @@ Tool::onRemove() {
   auto idsToRemove        = QMap<uint64_t, bool>{};
   auto emitRunningWarning = false;
 
-  m_model->withSelectedJobs(ui->jobs, [&idsToRemove](Job &job) { idsToRemove[job.m_id] = true; });
+  m_model->withSelectedJobs(ui->jobs, [&idsToRemove](Job &job) { idsToRemove[job.id()] = true; });
 
   m_model->removeJobsIf([&idsToRemove, &emitRunningWarning](Job const &job) -> bool {
-    if (!idsToRemove[job.m_id])
+    if (!idsToRemove[job.id()])
       return false;
-    if (Job::Running != job.m_status)
+    if (Job::Running != job.status())
       return true;
 
     emitRunningWarning = true;
@@ -206,16 +206,16 @@ Tool::onRemove() {
 void
 Tool::onRemoveDone() {
   m_model->removeJobsIf([this](Job const &job) {
-      return (Job::DoneOk       == job.m_status)
-          || (Job::DoneWarnings == job.m_status)
-          || (Job::Failed       == job.m_status)
-          || (Job::Aborted      == job.m_status);
+      return (Job::DoneOk       == job.status())
+          || (Job::DoneWarnings == job.status())
+          || (Job::Failed       == job.status())
+          || (Job::Aborted      == job.status());
     });
 }
 
 void
 Tool::onRemoveDoneOk() {
-  m_model->removeJobsIf([this](Job const &job) { return Job::DoneOk == job.m_status; });
+  m_model->removeJobsIf([this](Job const &job) { return Job::DoneOk == job.status(); });
 }
 
 void
@@ -223,7 +223,7 @@ Tool::onRemoveAll() {
   auto emitRunningWarning = false;
 
   m_model->removeJobsIf([&emitRunningWarning](Job const &job) -> bool {
-    if (Job::Running != job.m_status)
+    if (Job::Running != job.status())
       return true;
 
     emitRunningWarning = true;
@@ -323,7 +323,7 @@ Tool::stopQueue(bool immediately) {
 
   m_model->withAllJobs([](Job &job) {
     job.action([&job]() {
-      if (job.m_status == Job::PendingAuto)
+      if (job.status() == Job::PendingAuto)
         job.setPendingManual();
     });
   });
@@ -333,7 +333,7 @@ Tool::stopQueue(bool immediately) {
 
   m_model->withAllJobs([this, askBeforeAborting, &askedBeforeAborting](Job &job) {
     job.action([this, &job, askBeforeAborting, &askedBeforeAborting]() {
-      if (job.m_status != Job::Running)
+      if (job.status() != Job::Running)
         return;
 
       if (   !askBeforeAborting

src/mkvtoolnix-gui/main_window/main_window.cpp
@@ -312,8 +312,8 @@ MainWindow::beforeCloseCheckRunningJobs() {
 
   model->stop();
   model->withAllJobs([](Jobs::Job &job) {
-    if (Jobs::Job::Running == job.m_status) {
-      job.m_quitAfterFinished = true;
+    if (Jobs::Job::Running == job.status()) {
+      job.setQuitAfterFinished(true);
       job.abort();
     }
   });

src/mkvtoolnix-gui/watch_jobs/tool.cpp
@@ -80,7 +80,7 @@ Tool::toolShown() {
 
 void
 Tool::viewOutput(mtx::gui::Jobs::Job &job) {
-  auto idx = tabIndexForJobID(job.m_id);
+  auto idx = tabIndexForJobID(job.id());
   if (0 < idx) {
     ui->widgets->setCurrentIndex(idx);
     return;
@@ -90,7 +90,7 @@ Tool::viewOutput(mtx::gui::Jobs::Job &job) {
   tab->connectToJob(job);
   tab->setInitialDisplay(job);
 
-  ui->widgets->addTab(tab, job.m_description);
+  ui->widgets->addTab(tab, job.description());
   ui->widgets->setCurrentIndex(ui->widgets->count() - 1);
 }
 

commit e9bf713b92e69edcd193d7c079265e40d7ac35b3

GUI: job output: remember job ID when connecting
Was broken by ad40b74.

src/mkvtoolnix-gui/watch_jobs/tab.cpp
@@ -82,6 +82,7 @@ Tab::retranslateUi() {
 void
 Tab::connectToJob(Jobs::Job const &job) {
   m_currentlyConnectedJob = &job;
+  m_id                    = job.id();
   auto connType           = static_cast<Qt::ConnectionType>(Qt::AutoConnection | Qt::UniqueConnection);
 
   connect(&job, &Jobs::Job::statusChanged,   this, &Tab::onStatusChanged,      connType);