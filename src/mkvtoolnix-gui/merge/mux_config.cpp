88a3f6fd94a4da2abead4668f9b56b6c48df2a28
Moritz Bunkus <moritz@bunkus.org>
2015-08-18 22:11:01 +0200

    GUI: pass interface language to mkvmerge

--- src/mkvtoolnix-gui/merge/mux_config.cpp
+++ src/mkvtoolnix-gui/merge/mux_config.cpp
@@ -396,6 +396,8 @@ MuxConfig::buildMkvmergeOptions()
 
   auto &settings = Util::Settings::get();
 
+  options << Q("--ui-language") << settings.m_uiLocale;
+
   if (Util::Settings::NormalPriority != settings.m_priority)
     options << Q("--priority") << settings.priorityAsString();
 
