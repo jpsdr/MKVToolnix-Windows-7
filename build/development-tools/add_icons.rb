4e2daef026e341edd9f0b8122ea6d6f24c7a99c9
Moritz Bunkus <moritz@bunkus.org>
2015-08-27 22:48:07 +0200

    dev tools: script for adding icons to the corresponding directories

---
+++ build/development-tools/add_icons.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+# coding: utf-8
+
+require "fastimage"
+require "fileutils"
+
+$base_dir = File.absolute_path(File.dirname(__FILE__) + "/../..")
+
+ARGV.each do |file_name|
+  size = FastImage.size file_name
+  if !size
+    puts "Warning: couldn't determine size for #{file_name}"
+    next
+  end
+
+  dir = "#{$base_dir}/share/icons/#{size[0]}x#{size[1]}"
+  if !FileTest.exist? dir
+    puts "Warning: no destination directory for size #{size[0]}x#{size[1]}"
+    next
+  end
+
+  target = "#{dir}/" + file_name.gsub(/.*\//, '')
+
+  puts "#{file_name} → #{target}"
+  FileUtils.cp file_name, target
+end
+
+puts "Updating Qt resources"
+Dir.chdir $base_dir
+system "./drake dev:update-qt-resources"
