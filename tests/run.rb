c6a376cda200641d668d0f6f8b0c2e16b71f7b9d
Moritz Bunkus <moritz@bunkus.org>
2015-09-17 21:48:05 +0200

    tests run.rb: add help text

--- tests/run.rb
+++ tests/run.rb
@@ -47,13 +47,27 @@ def main
       controller.test_date_after = Time.local($1, $2, $3, $4, $5, $6)
     elsif (arg =~ /-D([0-9]{4})([0-9]{2})([0-9]{2})-([0-9]{2})([0-9]{2})/)
       controller.test_date_before = Time.local($1, $2, $3, $4, $5, $6)
-    elsif /^ (\d{3}) (?: - (\d{3}) )?$/x.match arg
-      $1.to_i.upto(($2 || $1).to_i) { |idx| controller.add_test_case sprintf("%03d", idx) }
-    elsif %r{^ / (.+) / $}x.match arg
-      re = Regexp.new "^T_(\\d+)#{$1}", Regexp::IGNORECASE
-      controller.results.results.keys.collect { |e| re.match(e) ? $1 : nil }.compact.each { |e| controller.add_test_case e }
     elsif arg =~ /-j(\d+)/
       controller.num_threads = $1.to_i
+    elsif /^ (\d{3}) (?: - (\d{3}) )?$/x.match arg
+      $1.to_i.upto(($2 || $1).to_i) { |idx| controller.add_test_case sprintf("%03d", idx) }
+    elsif %r{^ / (.+) / $}ix.match arg
+      re = Regexp.new "^T_(\\d+)#{$1}", Regexp::IGNORECASE
+      controller.results.results.keys.collect { |e| re.match(e) ? $1 : nil }.compact.each { |e| controller.add_test_case e }
+    elsif ((arg == "-h") || (arg == "--help"))
+      puts <<EOHELP
+Syntax: run.rb [options]
+  -f, --failed          only run tests marked as failed
+  -n, --new             only run tests for which no entry exists in results.txt
+  -dDATE                only run tests added after DATE (YYYYMMDD-HHMM)
+  -DDATE                only run tests added before DATE (YYYYMMDD-HHMM)
+  -u, --update-failed   update the results for tests that fail
+  -r, --record-duration update the duration field of the tests run
+  -jNUM                 run NUM tests at once (default: number of CPU cores)
+  123                   run test 123 (any number, can be given multiple times)
+  /REGEX/               run tests whose names match REGEX (case insensitive; can be given multiple times)
+EOHELP
+      exit 0
     else
       error_and_exit "Unknown argument '#{arg}'."
     end
