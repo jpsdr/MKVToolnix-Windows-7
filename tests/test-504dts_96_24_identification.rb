a348a727445e36a9d6dfe25d1dd1a3c672762f72
Moritz Bunkus <moritz@bunkus.org>
2015-10-06 22:45:56 +0200

    DTS: add a VOB to the 96/24 test case

--- tests/test-504dts_96_24_identification.rb
+++ tests/test-504dts_96_24_identification.rb
@@ -1,5 +1,4 @@
 #!/usr/bin/ruby -w
 
 describe "mkvmerge / identification of DTS 96/24"
-test_identify "data/dts/dts-96-24.dts"
-test_identify "data/dts/dts-96-24.mka"
+%w{dts-96-24.dts dts-96-24.mka dts-96-24.vob}.each { |file| test_identify "data/dts/#{file}" }
