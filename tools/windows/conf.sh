8764254b1dd23d31fd51af324955e3ea1462cdc8
Moritz Bunkus <moritz@bunkus.org>
2015-11-05 15:34:48 +0100

    saxon_process: inherit dir variables from parent script

--- tools/windows/conf.sh
+++ tools/windows/conf.sh
@@ -1,3 +1,5 @@
-mxe_dir=${HOME}/prog/video/mingw/cross
-saxon_dir=${HOME}/opt/saxon-he
-file_list_dir=${HOME}/prog/video/mingw/src/uc/file-lists
+mxe_dir=${mxe_dir:-${HOME}/prog/video/mingw/cross}
+saxon_dir=${saxon_dir:-${HOME}/opt/saxon-he}
+file_list_dir=${file_list_dir:-${HOME}/prog/video/mingw/src/uc/file-lists}
+
+export mxe_dir saxon_dir file_list_dir
