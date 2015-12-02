af306e90916c65d542ee676edab11099b11412aa
Moritz Bunkus <moritz@bunkus.org>
2015-12-02 18:23:18 +0100

    GUI: fix preferences tab order

--- src/mkvtoolnix-gui/main_window/preferences_dialog.cpp
+++ src/mkvtoolnix-gui/main_window/preferences_dialog.cpp
@@ -136,8 +136,8 @@ PreferencesDialog::setupPageSelector() {
                    addItem(page++, pGui,    QY("Often used selections"));
   auto pMerge    = addItem(page++, nullptr, QY("Merge"),             "merge");
                    addItem(page++, pMerge,  QY("Default values"));
-                   addItem(page++, pMerge,  QY("Enabling tracks"));
                    addItem(page++, pMerge,  QY("Output"));
+                   addItem(page++, pMerge,  QY("Enabling tracks"));
                    addItem(page++, pMerge,  QY("Playlists"));
                    addItem(page++, nullptr, QY("Chapter editor"),    "story-editor");
                    addItem(page++, nullptr, QY("Jobs & job queue"),  "view-task");
