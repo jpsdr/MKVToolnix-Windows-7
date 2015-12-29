jpsdr/MKVToolnix-Windows-7

commit aeb0add9b502f4bf58b8d88d5818752001e3be4d

GUI: watch jobs: use Qt's menu function of QPushButton
This makes it clearer visually that a menu will open when hte user
pushes the button.

src/mkvtoolnix-gui/watch_jobs/tab.cpp
@@ -37,6 +37,7 @@ class TabPrivate {
   Jobs::Job::Status m_currentJobStatus;
   QDateTime m_currentJobStartTime;
   QString m_currentJobDescription;
+  QMenu *m_moreActions;
 
   // Only use this variable for determining whether or not to ignore
   // certain signals.
@@ -50,6 +51,7 @@ class TabPrivate {
     , m_currentJobProgress{}
     , m_queueProgress{}
     , m_currentJobStatus{Jobs::Job::PendingManual}
+    , m_moreActions{new QMenu{tab}}
     , m_currentlyConnectedJob{}
     , m_saveOutputAction{new QAction{tab}}
     , m_clearOutputAction{new QAction{tab}}
@@ -83,18 +85,35 @@ Tab::setupUi() {
   for (auto const &splitter : findChildren<QSplitter *>())
     cfg.handleSplitterSizes(splitter);
 
+  setupMoreActionsMenu();
+
   retranslateUi();
 
   auto model = MainWindow::jobTool()->model();
 
   connect(d->ui->abortButton,                        &QPushButton::clicked,            this, &Tab::onAbort);
   connect(d->ui->acknowledgeWarningsAndErrorsButton, &QPushButton::clicked,            this, &Tab::acknowledgeWarningsAndErrors);
-  connect(d->ui->moreActionsButton,                  &QPushButton::clicked,            this, &Tab::showMoreActionsMenu);
   connect(model,                                     &Jobs::Model::progressChanged,    this, &Tab::onQueueProgressChanged);
   connect(model,                                     &Jobs::Model::queueStatusChanged, this, &Tab::updateRemainingTime);
+  connect(d->m_moreActions,                          &QMenu::aboutToShow,              this, &Tab::enableMoreActionsActions);
   connect(d->m_saveOutputAction,                     &QAction::triggered,              this, &Tab::onSaveOutput);
   connect(d->m_clearOutputAction,                    &QAction::triggered,              this, &Tab::clearOutput);
   connect(d->m_openFolderAction,                     &QAction::triggered,              this, &Tab::openFolder);
+}
+
+void
+Tab::setupMoreActionsMenu() {
+  Q_D(Tab);
+
+  // Setup the "more actions" menu.
+  d->m_moreActions->addAction(d->m_openFolderAction);
+  d->m_moreActions->addSeparator();
+  d->m_moreActions->addAction(d->m_saveOutputAction);
+
+  if (isCurrentJobTab())
+    d->m_moreActions->addAction(d->m_clearOutputAction);
+
+  d->ui->moreActionsButton->setMenu(d->m_moreActions);
 }
 
 void
@@ -430,23 +449,11 @@ Tab::openFolder() {
 }
 
 void
-Tab::showMoreActionsMenu() {
+Tab::enableMoreActionsActions() {
   Q_D(Tab);
-
-  QMenu menu{this};
 
   auto hasJob = std::numeric_limits<uint64_t>::max() != d->m_id;
   d->m_openFolderAction->setEnabled(hasJob);
-  d->m_clearOutputAction->setEnabled(true);
-
-  menu.addAction(d->m_openFolderAction);
-  menu.addSeparator();
-  menu.addAction(d->m_saveOutputAction);
-
-  if (isCurrentJobTab())
-    menu.addAction(d->m_clearOutputAction);
-
-  menu.exec(QCursor::pos());
 }
 
 }}}

src/mkvtoolnix-gui/watch_jobs/tab.h
@@ -63,10 +63,11 @@ public slots:
 
   void updateRemainingTime();
 
-  void showMoreActionsMenu();
+  void enableMoreActionsActions();
 
 protected:
   void setupUi();
+  void setupMoreActionsMenu();
 
   static void updateOneRemainingTimeLabel(QLabel *label, QDateTime const &startTime, uint64_t progress);
 };

commit cb0e958eb6adb3b489eb8d46462f8fba32d1ae1c

GUI: watch jobs: re-add "clear output" in "more actions" menu
This regression was introduced in aeb0add.

The new menu is created during the tab's constructor. At that point it
called "isCurrentJobTab()" do determine whether or not to add the "clear
outputs" action. However, "isCurrentJobTab()" relied on the MainWindow's
currentJobTab variable being set, which at that point isn't yet as the
tab is still being constructed.

Solve this by passing a static parameter to the tabs that indicate
whether or not they're the "current job" tab.

src/mkvtoolnix-gui/watch_jobs/tab.cpp
@@ -38,6 +38,7 @@ class TabPrivate {
   QDateTime m_currentJobStartTime;
   QString m_currentJobDescription;
   QMenu *m_moreActions;
+  bool m_forCurrentJob;
 
   // Only use this variable for determining whether or not to ignore
   // certain signals.
@@ -45,13 +46,14 @@ class TabPrivate {
 
   QAction *m_saveOutputAction, *m_clearOutputAction, *m_openFolderAction;
 
-  explicit TabPrivate(Tab *tab)
+  explicit TabPrivate(Tab *tab, bool forCurrentJob)
     : ui{new Ui::Tab}
     , m_id{std::numeric_limits<uint64_t>::max()}
     , m_currentJobProgress{}
     , m_queueProgress{}
     , m_currentJobStatus{Jobs::Job::PendingManual}
     , m_moreActions{new QMenu{tab}}
+    , m_forCurrentJob{forCurrentJob}
     , m_currentlyConnectedJob{}
     , m_saveOutputAction{new QAction{tab}}
     , m_clearOutputAction{new QAction{tab}}
@@ -60,9 +62,10 @@ class TabPrivate {
   }
 };
 
-Tab::Tab(QWidget *parent)
+Tab::Tab(QWidget *parent,
+         bool forCurrentJob)
   : QWidget{parent}
-  , d_ptr{new TabPrivate{this}}
+  , d_ptr{new TabPrivate{this, forCurrentJob}}
 {
   setupUi();
 }
@@ -406,7 +409,9 @@ Tab::isSaveOutputEnabled()
 bool
 Tab::isCurrentJobTab()
   const {
-  return this == MainWindow::get()->watchCurrentJobTab();
+  Q_D(const Tab);
+
+  return d->m_forCurrentJob;
 }
 
 void

src/mkvtoolnix-gui/watch_jobs/tab.h
@@ -28,7 +28,7 @@ private:
   QScopedPointer<TabPrivate> const d_ptr;
 
 public:
-  explicit Tab(QWidget *parent);
+  explicit Tab(QWidget *parent, bool forCurrentJob = false);
   ~Tab();
 
   virtual void retranslateUi();

src/mkvtoolnix-gui/watch_jobs/tool.cpp
@@ -33,7 +33,7 @@ void
 Tool::setupUi() {
   setupTabPositions();
 
-  m_currentJobTab = new Tab{ui->widgets};
+  m_currentJobTab = new Tab{ui->widgets, true};
   ui->widgets->insertTab(0, m_currentJobTab, Q(""));
 
   auto button = Util::tabWidgetCloseTabButton(*ui->widgets, 0);