4cacf70c6ed2b86f0cd0a13727c9b715617f5e6d
Moritz Bunkus <moritz@bunkus.org>
2016-03-02 13:10:12 +0100

    tests: add cases for generating chapters without video tracks

---
+++ tests/test-534chapter_generation_when_appending_audio_only.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/ruby -w
+# coding: utf-8
+
+file = "data/simple/v.mp3"
+
+# T_534chapter_generation_when_appending_audio_only
+describe "mkvmerge / generate chapter »when-appending« without video tracks"
+
+def hash_results max
+  ( (1..max).collect { |i| hash_file(sprintf("%s-%02d", tmp, i)) } + [ File.exists?(sprintf("%s-%02d", tmp, max + 1)) ? 'bad' : 'ok' ]).join '+'
+end
+
+test_merge "#{file} + #{file} + #{file}", :args => "--generate-chapters when-appending"
+test_merge "#{file} + #{file} + #{file}", :args => "--chapter-language ger --generate-chapters when-appending"
+test "creation and splitting" do
+  merge "--chapter-language ger --generate-chapters when-appending --split 30s #{file} + #{file} + #{file}", :output => "#{tmp}-%02d"
+  hash_results 7
+end
+test_merge "#{file}", :args => "--generate-chapters when-appending"
---
+++ tests/test-535chapter_generation_interval_audio_only.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/ruby -w
+# coding: utf-8
+
+file = "data/simple/v.mp3"
+
+# T_535chapter_generation_interval_audio_only
+describe "mkvmerge / generate chapter »interval« without video tracks"
+
+def hash_results max
+  ( (1..max).collect { |i| hash_file(sprintf("%s-%02d", tmp, i)) } + [ File.exists?(sprintf("%s-%02d", tmp, max + 1)) ? 'bad' : 'ok' ]).join '+'
+end
+
+test_merge "#{file} + #{file} + #{file}", :args => "--generate-chapters interval:30s"
+test_merge "#{file} + #{file} + #{file}", :args => "--chapter-language ger --generate-chapters interval:30s"
+test "creation and splitting" do
+  merge "--chapter-language ger --generate-chapters interval:30s --split 30s #{file} + #{file} + #{file}", :output => "#{tmp}-%02d"
+  hash_results 7
+end
+test_merge "#{file}", :args => "--generate-chapters interval:30s"
