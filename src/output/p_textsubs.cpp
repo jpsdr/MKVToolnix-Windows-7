jpsdr/MKVToolnix-Windows-7

commit 8ca1160de08bd281cf707cda75417dea1d80ec51

SRT, SSA readers: ignore --sub-charset for files containing a BOM
Fixes #1620.

src/output/p_textsubs.cpp
@@ -36,11 +36,10 @@ textsubs_packetizer_c::textsubs_packetizer_c(generic_reader_c *p_reader,
                                              bool is_utf8)
   : generic_packetizer_c(p_reader, p_ti)
   , m_packetno{}
-  , m_codec_id(codec_id)
-  , m_recode(recode)
+  , m_codec_id{codec_id}
 {
-  if (m_recode)
-    m_cc_utf8 = charset_converter_c::init((m_ti.m_sub_charset != "") || !is_utf8 ? m_ti.m_sub_charset : "UTF-8");
+  if (recode && !is_utf8 && !m_ti.m_sub_charset.empty())
+    m_cc_utf8 = charset_converter_c::init(m_ti.m_sub_charset);
 
   set_track_type(track_subtitle);
   if (m_codec_id == MKV_S_TEXTUSF)
@@ -93,7 +92,7 @@ textsubs_packetizer_c::process(packet_cptr packet) {
   subs = boost::regex_replace(subs, s_re_remove_trailing_nl, "",     boost::match_default | boost::match_single_line);
   subs = boost::regex_replace(subs, s_re_translate_nl,       "\r\n", boost::match_default | boost::match_single_line);
 
-  if (m_recode)
+  if (m_cc_utf8)
     subs = m_cc_utf8->utf8(subs);
 
   packet->data = memory_cptr(new memory_c((unsigned char *)subs.c_str(), subs.length(), false));

src/input/r_ssa.cpp
@@ -48,9 +48,9 @@ ssa_reader_c::read_headers() {
   if (!ssa_reader_c::probe_file(text_in.get(), 0))
     throw mtx::input::invalid_format_x();
 
-  charset_converter_cptr cc_utf8 = mtx::includes(m_ti.m_sub_charsets,  0) ? charset_converter_c::init(m_ti.m_sub_charsets[ 0])
+  charset_converter_cptr cc_utf8 = text_in->get_byte_order() != BO_NONE   ? charset_converter_c::init("UTF-8")
+                                 : mtx::includes(m_ti.m_sub_charsets,  0) ? charset_converter_c::init(m_ti.m_sub_charsets[ 0])
                                  : mtx::includes(m_ti.m_sub_charsets, -1) ? charset_converter_c::init(m_ti.m_sub_charsets[-1])
-                                 : text_in->get_byte_order() != BO_NONE   ? charset_converter_c::init("UTF-8")
                                  :                                          g_cc_local_utf8;
 
   m_ti.m_id = 0;

src/output/p_textsubs.h
@@ -24,7 +24,6 @@ private:
   boost::optional<unsigned int> m_force_rerender_track_headers_on_packetno;
   charset_converter_cptr m_cc_utf8;
   std::string m_codec_id;
-  bool m_recode;
 
 public:
   textsubs_packetizer_c(generic_reader_c *p_reader, track_info_c &p_ti, const char *codec_id, bool recode, bool is_utf8);

tests/test-537srt_bom_precedence_over_sub_charset.rb
@@ -1,0 +1,9 @@
+#!/usr/bin/ruby -w
+
+# T_537srt_bom_precedence_over_sub_charset
+describe "mkvmerge / SRT: BOMs have precedence over --sub-charset"
+
+test_merge "--sub-charset 0:ISO-8859-15 data/subtitles/srt/vde-utf-8-bom.srt"
+test_merge "                            data/subtitles/srt/vde-utf-8-bom.srt"
+test_merge "--sub-charset 0:ISO-8859-15 data/subtitles/srt/vde.srt"
+test_merge "                            data/subtitles/srt/vde.srt"

commit ff42108c44af472f51974b31aa8a6d75ff7b6996

subtitle packetizer: assume system character set if none is given
This is a regression introduced in
8ca1160de08bd281cf707cda75417dea1d80ec51 which was supposed to fix
--sub-charset having an effect on files with a byte-order
mark (BOM).

Problem is that the supposed fix also made the subtitle packetizer only
recode if a character set was explicitly given even for files which do
not contain a BOM. Instead it should recode from the system's character
set in such a case. This is done if charset_converter_c::init() is
called with an empty string. Unfortunately commit 8ca1160 changed the
packetizer not to call charset_converter_c::init() if the given
character set is empty.

Thee semantics are:

- "recode" is not set if the source is known to be in UTF-8 already,
  e.g. if the track is read from an SRT file with a BOM or if it's read
  from a Matroska file.
- "is_utf8" is set if the source is known to be in UTF-8 for other
  reasons.

Therefore if recode is set and is_utf8 isn't then the conversion must
take place no matter if --sub-charset is given or not. If it is
given (m_ti.m_sub_charset is not empty) then use that;
otherwise (m__ti.m_sub_charset is empty) use the system's character
set.

Fixes #1639.

src/output/p_textsubs.cpp
@@ -38,7 +38,7 @@ textsubs_packetizer_c::textsubs_packetizer_c(generic_reader_c *p_reader,
   , m_packetno{}
   , m_codec_id{codec_id}
 {
-  if (recode && !is_utf8 && !m_ti.m_sub_charset.empty())
+  if (recode && !is_utf8)
     m_cc_utf8 = charset_converter_c::init(m_ti.m_sub_charset);
 
   set_track_type(track_subtitle);