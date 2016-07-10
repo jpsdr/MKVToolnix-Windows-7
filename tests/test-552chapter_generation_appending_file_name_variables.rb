3f92246ee9fe3ff8ce8ae1e421d725763952ff20
Moritz Bunkus <moritz@bunkus.org>
2016-07-10 20:34:25 +0200

    tests: case for chapter generation when appending with file name variables

---
+++ tests/test-552chapter_generation_appending_file_name_variables.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/ruby -w
+# coding: utf-8
+
+base = "data/simple/Datei didödeldü "
+
+# T_552chapter_generation_appending_file_name_variables
+describe "mkvmerge / chapter generation when appending; testing file name variables"
+test_merge "'#{base}1.mp3' + '#{base}2.mp3' + '#{base}3.mp3' --generate-chapters when-appending --generate-chapters-name-template 'File name: <FILE_NAME>; with extension: <FILE_NAME_WITH_EXT>'"
