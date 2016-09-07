2b9d5fbc51f654c164a2be1aec1c11b3d2894d09
Moritz Bunkus <moritz@bunkus.org>
2016-09-07 21:55:14 +0200

    tests: don't show mkvmerge's output

--- tests/test-552chapter_generation_appending_file_name_variables.rb
+++ tests/test-552chapter_generation_appending_file_name_variables.rb
@@ -5,4 +5,4 @@ base = "data/simple/Datei didödeldü "
 
 # T_552chapter_generation_appending_file_name_variables
 describe "mkvmerge / chapter generation when appending; testing file name variables"
-test_merge "'#{base}1.mp3' + '#{base}2.mp3' + '#{base}3.mp3' --generate-chapters when-appending --generate-chapters-name-template 'File name: <FILE_NAME>; with extension: <FILE_NAME_WITH_EXT>'"
+test_merge "'#{base}1.mp3' + '#{base}2.mp3' + '#{base}3.mp3' --generate-chapters when-appending --generate-chapters-name-template 'File name: <FILE_NAME>; with extension: <FILE_NAME_WITH_EXT>' > /dev/null"
