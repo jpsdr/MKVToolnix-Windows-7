jpsdr/MKVToolnix-Windows-7

commit 508c925eef8c30c228a24a474965544ff0ffb185

AVC, HEVC reader: require detected width/height to be positive
Otherwise the VideoPixelWidth/Height elements won't be set causing an
assert()ion to fail in libMatroska when rendering the track headers.

Fixes the last test case from #1780:

explorer:id:000776,sig:06,src:002706,op:havoc,rep:4

src/input/r_avc.cpp
@@ -60,8 +60,13 @@ avc_es_reader_c::probe_file(mm_io_c *in,
 
       parser.add_bytes(buf->get_buffer(), num_read);
 
-      if (parser.headers_parsed())
-        return 1;
+      if (!parser.headers_parsed())
+        return 0;
+
+      if ((parser.get_width() <= 0) || (parser.get_height() <= 0))
+        return 0;
+
+      return 1;
     }
 
   } catch (mtx::exception &e) {
@@ -103,6 +108,8 @@ avc_es_reader_c::read_headers() {
 
     m_width  = parser.get_width();
     m_height = parser.get_height();
+
+    mxinfo(boost::format("width %1% height %2%\n") % m_width % m_height);
 
     m_in->setFilePointer(0, seek_beginning);
 

src/input/r_hevc.cpp
@@ -55,8 +55,13 @@ hevc_es_reader_c::probe_file(mm_io_c *in,
 
       parser.add_bytes(buf->get_buffer(), num_read);
 
-      if (parser.headers_parsed())
-        return 1;
+      if (!parser.headers_parsed())
+        return 0;
+
+      if ((parser.get_width() <= 0) || (parser.get_height() <= 0))
+        return 0;
+
+      return 1;
     }
 
   } catch (...) {

commit 190cd56e7b4c795bd70e8eae2b252dae6aa7af16

AVC, HEVC readers: fix requiring detected width/height to be positive
Under certain conditions the width/height of hadn't been known in the
HEVC when the HEVCC was considered to be ready. The width/height were
only fixed by later SPS instances in the bitstream.

In that case the HEVC reader was only reading until the the parser
signaled that the HEVCC was ready. The following check for width &
height being positive failed, causing detection to fail.

The change is to let the HEVC parser signal readiness if and only if
both the HEVCC is ready and width & height are positive.

The same applies to the AVC reader.

This fixes the regression introduced in
508c925eef8c30c228a24a474965544ff0ffb185.

src/common/hevc.cpp
@@ -1452,6 +1452,15 @@ es_parser_c::~es_parser_c() {
       mxdebug(boost::format("  %1%: %2%\n") % s_type_names[i] % m_stats.num_slices_by_type[i]);
 }
 
+bool
+es_parser_c::headers_parsed()
+  const {
+  return m_hevcc_ready
+      && !m_sps_info_list.empty()
+      && (m_sps_info_list.front().width  > 0)
+      && (m_sps_info_list.front().height > 0);
+}
+
 void
 es_parser_c::discard_actual_frames(bool discard) {
   m_discard_actual_frames = discard;

src/common/hevc.h
@@ -538,9 +538,7 @@ public:
 
   void add_timecode(int64_t timecode);
 
-  bool headers_parsed() const {
-    return m_hevcc_ready;
-  }
+  bool headers_parsed() const;
 
   void set_nalu_size_length(int nalu_size_length) {
     m_nalu_size_length = nalu_size_length;

src/common/mpeg4_p10.cpp
@@ -936,6 +936,15 @@ mpeg4::p10::avc_es_parser_c::~avc_es_parser_c() {
       mxdebug(boost::format("  %1%: %2%\n") % s_type_names[i] % m_stats.num_slices_by_type[i]);
 }
 
+bool
+mpeg4::p10::avc_es_parser_c::headers_parsed()
+  const {
+  return m_avcc_ready
+      && !m_sps_info_list.empty()
+      && (m_sps_info_list.front().width  > 0)
+      && (m_sps_info_list.front().height > 0);
+}
+
 void
 mpeg4::p10::avc_es_parser_c::discard_actual_frames(bool discard) {
   m_discard_actual_frames = discard;

src/common/mpeg4_p10.h
@@ -346,9 +346,7 @@ public:
 
   void add_timecode(int64_t timecode);
 
-  bool headers_parsed() const {
-    return m_avcc_ready;
-  }
+  bool headers_parsed() const;
 
   void set_nalu_size_length(int nalu_size_length) {
     m_nalu_size_length = nalu_size_length;

src/input/r_avc.cpp
@@ -60,13 +60,8 @@ avc_es_reader_c::probe_file(mm_io_c *in,
 
       parser.add_bytes(buf->get_buffer(), num_read);
 
-      if (!parser.headers_parsed())
-        return 0;
-
-      if ((parser.get_width() <= 0) || (parser.get_height() <= 0))
-        return 0;
-
-      return 1;
+      if (parser.headers_parsed())
+        return 1;
     }
 
   } catch (mtx::exception &e) {

src/input/r_hevc.cpp
@@ -55,13 +55,8 @@ hevc_es_reader_c::probe_file(mm_io_c *in,
 
       parser.add_bytes(buf->get_buffer(), num_read);
 
-      if (!parser.headers_parsed())
-        return 0;
-
-      if ((parser.get_width() <= 0) || (parser.get_height() <= 0))
-        return 0;
-
-      return 1;
+      if (parser.headers_parsed())
+        return 1;
     }
 
   } catch (...) {

tests/test-565hevc_detection_failed_due_to_width_height.rb
@@ -1,0 +1,9 @@
+#!/usr/bin/ruby -w
+
+# T_565hevc_detection_failed_due_to_width_height
+describe "mkvmerge / HEVC detection failed due to width/height not being said at the wrong point in time"
+
+file = "data/h265/x265_v2.0+24_crf12_aqmode0_threads1_hightier.h265"
+
+test_identify file
+test_merge file