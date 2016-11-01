jpsdr/MKVToolnix-Windows-7

commit e06d80f70b564ef130de8eee53f551b80d91da31

merge: force-pull all packetizers from a fully-held file
In its core mkvmerge uses a pull model: it iterates over each packetizer
and asks it to produce a packet. The packetizer passes that request on
to the reader it belongs to. The reader will then read the next frame
for the track corresponding to the packetizer and pass that data back to
the packetizer.

However, several container formats (e.g. MPEG PS, Matroska, Ogg and
others) do not allow arbitrary access to the frames for each
track. Instead mkvmerge has to read them from front to back. In such a
case the reader ignores the packetizer and simply reads the next frame,
whichever packetizer that might belong to.

This is problematic with sparse tracks. For those the packetizer will
repeatedly call the reader's read() method, but the read() method will
only produce data for the other tracks in that file. That data is then
buffered in those tracks' packetizers.

In order to limit that memory usage the readers/packetizers know a state
known as "holding" — meaning a lot of data has been buffered and no data
for this packetizer has been found yet.

The actual problem occurred when all packetizers for a file were holding
and no packets were present for any of those packetizers:

- The packetizers run empty on data.
- The reader sees too much data had been buffered and enters "holding"
  state.
- The core only saw packetizers that don't have any data anymore and
  concluded that it was done.

This commit changes the behavior to force a pull in such a
situation (which the readers already provided for — it was just that the
code never opted to force the pull). This will allow the reader to
exceed its allotted memory usage and deliver more data.

src/merge/output_control.cpp
@@ -1819,7 +1819,7 @@ append_track(packetizer_t &ptzr,
   else if (   (ptzr.packetizer->get_track_type() == track_subtitle)
            || (src_file.reader->m_chapters)) {
     if (!src_file.reader->m_ptzr_first_packet)
-      ptzr.status = ptzr.packetizer->read();
+      ptzr.status = ptzr.packetizer->read(false);
 
     if (src_file.reader->m_ptzr_first_packet) {
       auto cmp_amap = brng::find_if(g_append_mapping, [&amap, &src_file](append_spec_t const &m) {
@@ -1916,6 +1916,31 @@ establish_deferred_connections(filelist_t &file) {
   // \todo Select a new file that the subs will defer to.
 }
 
+static bool
+force_pull_packetizers_of_fully_held_files() {
+  std::unordered_map<generic_reader_c *, bool> fully_held_files;
+
+  for (auto &ptzr : g_packetizers) {
+    auto reader = ptzr.packetizer->m_reader;
+    auto pos    = fully_held_files.find(reader);
+
+    if (pos == fully_held_files.end())
+      fully_held_files[reader] = true;
+
+    if (FILE_STATUS_HOLDING != ptzr.status)
+      fully_held_files[reader] = false;
+  }
+
+  auto force_pulled = false;
+  for (auto &ptzr : g_packetizers)
+    if (fully_held_files[ptzr.packetizer->m_reader] && !ptzr.packetizer->packet_available()) {
+      ptzr.packetizer->read(true);
+      force_pulled = true;
+    }
+
+  return force_pulled;
+}
+
 static void
 pull_packetizers_for_packets() {
   for (auto &ptzr : g_packetizers) {
@@ -1927,7 +1952,7 @@ pull_packetizers_for_packets() {
     while (   !ptzr.pack
            && (FILE_STATUS_MOREDATA == ptzr.status)
            && !ptzr.packetizer->packet_available())
-      ptzr.status = ptzr.packetizer->read();
+      ptzr.status = ptzr.packetizer->read(false);
 
     if (   (FILE_STATUS_MOREDATA != ptzr.status)
            && (FILE_STATUS_MOREDATA == ptzr.old_status))
@@ -1997,6 +2022,7 @@ main_loop() {
     // Step 1: Make sure a packet is available for each output
     // as long we haven't already processed the last one.
     pull_packetizers_for_packets();
+    auto force_pulled = force_pull_packetizers_of_fully_held_files();
 
     // Step 2: Pick the packet with the lowest timecode and
     // stuff it into the Matroska file.
@@ -2025,7 +2051,7 @@ main_loop() {
       if (1 <= verbose)
         display_progress();
 
-    } else if (!appended_a_track) // exit if there are no more packets
+    } else if (!appended_a_track && !force_pulled) // exit if there are no more packets
       break;
   }
 

src/merge/generic_packetizer.cpp
@@ -1563,8 +1563,8 @@ generic_packetizer_c::create_track_number() {
 }
 
 file_status_e
-generic_packetizer_c::read() {
-  return m_reader->read(this);
+generic_packetizer_c::read(bool force) {
+  return m_reader->read(this, force);
 }
 
 void

src/merge/generic_packetizer.h
@@ -103,7 +103,7 @@ public:
 
   virtual bool contains_gap();
 
-  virtual file_status_e read();
+  virtual file_status_e read(bool force);
 
   inline void add_packet(packet_t *packet) {
     add_packet(packet_cptr(packet));

commit 2e76c2e251b1c2ebddaecf922234f1faa4000c81

mkvmerge: handle end-of-input in "force pulling" function, too
Otherwise a situation may arise where a file still in a non-finished
state going into the force pull function and coming out of it in a
finished state. However, mkvmerge has to do some work on such a
transition:

• housekeeping (decrease number of unfinished packetizers for later
  decisions whether or not a file has been read)
• establishing deferred connections when appending

In such a case mkvmerge entered an endless loop.

This was a regression introduced with commit
e06d80f70b564ef130de8eee53f551b80d91da31 as a fix to #1774.

src/merge/output_control.cpp
@@ -1916,6 +1916,30 @@ establish_deferred_connections(filelist_t &file) {
   // \todo Select a new file that the subs will defer to.
 }
 
+static void
+check_and_handle_end_of_input_after_pulling(packetizer_t &ptzr) {
+  if (!ptzr.pack && (FILE_STATUS_DONE == ptzr.status))
+    ptzr.status = FILE_STATUS_DONE_AND_DRY;
+
+  // Has this packetizer changed its status from "data available" to
+  // "file done" during this loop? If so then decrease the number of
+  // unfinished packetizers in the corresponding file structure.
+  if (   (FILE_STATUS_DONE_AND_DRY != ptzr.status)
+      || (ptzr.old_status          == ptzr.status))
+    return;
+
+  auto &file = *g_files[ptzr.file];
+  file.num_unfinished_packetizers--;
+
+  // If all packetizers for a file have finished then establish the
+  // deferred connections.
+  if ((0 >= file.num_unfinished_packetizers) && (0 < file.old_num_unfinished_packetizers)) {
+    establish_deferred_connections(file);
+    file.done = true;
+  }
+  file.old_num_unfinished_packetizers = file.num_unfinished_packetizers;
+}
+
 static bool
 force_pull_packetizers_of_fully_held_files() {
   std::unordered_map<generic_reader_c *, bool> fully_held_files;
@@ -1934,8 +1958,14 @@ force_pull_packetizers_of_fully_held_files() {
   auto force_pulled = false;
   for (auto &ptzr : g_packetizers)
     if (fully_held_files[ptzr.packetizer->m_reader] && !ptzr.packetizer->packet_available()) {
-      ptzr.packetizer->read(true);
-      force_pulled = true;
+      ptzr.old_status = ptzr.status;
+      ptzr.status     = ptzr.packetizer->read(true);
+      force_pulled    = true;
+
+      if (!ptzr.pack)
+        ptzr.pack = ptzr.packetizer->get_packet();
+
+      check_and_handle_end_of_input_after_pulling(ptzr);
     }
 
   return force_pulled;
@@ -1955,31 +1985,13 @@ pull_packetizers_for_packets() {
       ptzr.status = ptzr.packetizer->read(false);
 
     if (   (FILE_STATUS_MOREDATA != ptzr.status)
-           && (FILE_STATUS_MOREDATA == ptzr.old_status))
+        && (FILE_STATUS_MOREDATA == ptzr.old_status))
       ptzr.packetizer->force_duration_on_last_packet();
 
     if (!ptzr.pack)
       ptzr.pack = ptzr.packetizer->get_packet();
 
-    if (!ptzr.pack && (FILE_STATUS_DONE == ptzr.status))
-      ptzr.status = FILE_STATUS_DONE_AND_DRY;
-
-    // Has this packetizer changed its status from "data available" to
-    // "file done" during this loop? If so then decrease the number of
-    // unfinished packetizers in the corresponding file structure.
-    if (   (FILE_STATUS_DONE_AND_DRY == ptzr.status)
-        && (ptzr.old_status != ptzr.status)) {
-      auto &file = *g_files[ptzr.file];
-      file.num_unfinished_packetizers--;
-
-      // If all packetizers for a file have finished then establish the
-      // deferred connections.
-      if ((0 >= file.num_unfinished_packetizers) && (0 < file.old_num_unfinished_packetizers)) {
-        establish_deferred_connections(file);
-        file.done = true;
-      }
-      file.old_num_unfinished_packetizers = file.num_unfinished_packetizers;
-    }
+    check_and_handle_end_of_input_after_pulling(ptzr);
   }
 }
 