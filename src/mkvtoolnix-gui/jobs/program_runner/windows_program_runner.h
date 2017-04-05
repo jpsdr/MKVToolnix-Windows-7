5a516baf027f710d6d92e06cd09ca7120124cbc8
Moritz Bunkus <moritz@bunkus.org>
2017-04-05 13:37:40 +0200

    GUI: cosmetics: namespaces all on one line

--- src/mkvtoolnix-gui/jobs/program_runner/windows_program_runner.h
+++ src/mkvtoolnix-gui/jobs/program_runner/windows_program_runner.h
@@ -7,9 +7,7 @@
 
 #include "mkvtoolnix-gui/jobs/program_runner.h"
 
-namespace mtx { namespace gui {
-
-namespace Jobs {
+namespace mtx { namespace gui { namespace Jobs {
 
 class WindowsProgramRunner: public ProgramRunner {
   Q_OBJECT;
