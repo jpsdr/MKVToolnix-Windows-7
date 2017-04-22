c008f208649c2b3a5514e772a0ac3f14f07b2fac
Moritz Bunkus <moritz@bunkus.org>
2017-04-22 15:19:14 +0200

    tests: cosmetics (alignment)

--- tests/unit/common/version.cpp
+++ tests/unit/common/version.cpp
@@ -87,63 +87,63 @@ TEST(VersionNumberT, ParsingInvalid) {
 }
 
 TEST(VersionNumberT, Compare) {
-  EXPECT_EQ(0, version_number_t{"1.2.3"}.compare(version_number_t{"1.2.3"}));
-  EXPECT_EQ(0, version_number_t{"1.2.3.0"}.compare(version_number_t{"1.2.3"}));
-  EXPECT_EQ(0, version_number_t{"1.2.3"}.compare(version_number_t{"1.2.3.0"}));
-  EXPECT_EQ(0, version_number_t{"1.2.3"}.compare(version_number_t{"1.2.3 build 0"}));
-  EXPECT_EQ(0, version_number_t{"1.2.3.0"}.compare(version_number_t{"1.2.3 build 0"}));
-  EXPECT_EQ(0, version_number_t{"11"}.compare(version_number_t{"11.0.0.0"}));
+  EXPECT_EQ(0, version_number_t{"1.2.3"}         .compare(version_number_t{"1.2.3"}));
+  EXPECT_EQ(0, version_number_t{"1.2.3.0"}       .compare(version_number_t{"1.2.3"}));
+  EXPECT_EQ(0, version_number_t{"1.2.3"}         .compare(version_number_t{"1.2.3.0"}));
+  EXPECT_EQ(0, version_number_t{"1.2.3"}         .compare(version_number_t{"1.2.3 build 0"}));
+  EXPECT_EQ(0, version_number_t{"1.2.3.0"}       .compare(version_number_t{"1.2.3 build 0"}));
+  EXPECT_EQ(0, version_number_t{"11"}            .compare(version_number_t{"11.0.0.0"}));
 
-  EXPECT_EQ(1, version_number_t{"1.2.3"}.compare(version_number_t{"1.0.0"}));
-  EXPECT_EQ(1, version_number_t{"1.2.4"}.compare(version_number_t{"1.2.3.9"}));
-  EXPECT_EQ(1, version_number_t{"1.2.10"}.compare(version_number_t{"1.2.9"}));
-  EXPECT_EQ(1, version_number_t{"1.2.3 build 1"}.compare(version_number_t{"1.2.3"}));
+  EXPECT_EQ(1, version_number_t{"1.2.3"}         .compare(version_number_t{"1.0.0"}));
+  EXPECT_EQ(1, version_number_t{"1.2.4"}         .compare(version_number_t{"1.2.3.9"}));
+  EXPECT_EQ(1, version_number_t{"1.2.10"}        .compare(version_number_t{"1.2.9"}));
+  EXPECT_EQ(1, version_number_t{"1.2.3 build 1"} .compare(version_number_t{"1.2.3"}));
   EXPECT_EQ(1, version_number_t{"1.2.3 build 10"}.compare(version_number_t{"1.2.3 build 9"}));
-  EXPECT_EQ(1, version_number_t{"11"}.compare(version_number_t{"9.9.9"}));
+  EXPECT_EQ(1, version_number_t{"11"}            .compare(version_number_t{"9.9.9"}));
 
-  EXPECT_EQ(-1, version_number_t{"1.0.0"}.compare(version_number_t{"1.2.3"}));
-  EXPECT_EQ(-1, version_number_t{"1.2.3.9"}.compare(version_number_t{"1.2.4"}));
-  EXPECT_EQ(-1, version_number_t{"1.2.9"}.compare(version_number_t{"1.2.10"}));
-  EXPECT_EQ(-1, version_number_t{"1.2.3"}.compare(version_number_t{"1.2.3 build 1"}));
+  EXPECT_EQ(-1, version_number_t{"1.0.0"}        .compare(version_number_t{"1.2.3"}));
+  EXPECT_EQ(-1, version_number_t{"1.2.3.9"}      .compare(version_number_t{"1.2.4"}));
+  EXPECT_EQ(-1, version_number_t{"1.2.9"}        .compare(version_number_t{"1.2.10"}));
+  EXPECT_EQ(-1, version_number_t{"1.2.3"}        .compare(version_number_t{"1.2.3 build 1"}));
   EXPECT_EQ(-1, version_number_t{"1.2.3 build 9"}.compare(version_number_t{"1.2.3 build 10"}));
-  EXPECT_EQ(-1, version_number_t{"9.9.9"}.compare(version_number_t{"11"}));
+  EXPECT_EQ(-1, version_number_t{"9.9.9"}        .compare(version_number_t{"11"}));
 }
 
 TEST(VersionNumberT, LessThan) {
-  EXPECT_FALSE(version_number_t{"1.2.3"} < version_number_t{"1.2.3"});
-  EXPECT_FALSE(version_number_t{"1.2.3.0"} < version_number_t{"1.2.3"});
-  EXPECT_FALSE(version_number_t{"1.2.3"} < version_number_t{"1.2.3.0"});
-  EXPECT_FALSE(version_number_t{"1.2.3"} < version_number_t{"1.2.3 build 0"});
-  EXPECT_FALSE(version_number_t{"1.2.3.0"} < version_number_t{"1.2.3 build 0"});
-  EXPECT_FALSE(version_number_t{"11"} < version_number_t{"11.0.0.0"});
+  EXPECT_FALSE(version_number_t{"1.2.3"}          < version_number_t{"1.2.3"});
+  EXPECT_FALSE(version_number_t{"1.2.3.0"}        < version_number_t{"1.2.3"});
+  EXPECT_FALSE(version_number_t{"1.2.3"}          < version_number_t{"1.2.3.0"});
+  EXPECT_FALSE(version_number_t{"1.2.3"}          < version_number_t{"1.2.3 build 0"});
+  EXPECT_FALSE(version_number_t{"1.2.3.0"}        < version_number_t{"1.2.3 build 0"});
+  EXPECT_FALSE(version_number_t{"11"}             < version_number_t{"11.0.0.0"});
 
-  EXPECT_FALSE(version_number_t{"1.2.3"} < version_number_t{"1.0.0"});
-  EXPECT_FALSE(version_number_t{"1.2.4"} < version_number_t{"1.2.3.9"});
-  EXPECT_FALSE(version_number_t{"1.2.10"} < version_number_t{"1.2.9"});
-  EXPECT_FALSE(version_number_t{"1.2.3 build 1"} < version_number_t{"1.2.3"});
+  EXPECT_FALSE(version_number_t{"1.2.3"}          < version_number_t{"1.0.0"});
+  EXPECT_FALSE(version_number_t{"1.2.4"}          < version_number_t{"1.2.3.9"});
+  EXPECT_FALSE(version_number_t{"1.2.10"}         < version_number_t{"1.2.9"});
+  EXPECT_FALSE(version_number_t{"1.2.3 build 1"}  < version_number_t{"1.2.3"});
   EXPECT_FALSE(version_number_t{"1.2.3 build 10"} < version_number_t{"1.2.3 build 9"});
-  EXPECT_FALSE(version_number_t{"11"} < version_number_t{"9.9.9"});
+  EXPECT_FALSE(version_number_t{"11"}             < version_number_t{"9.9.9"});
 
-  EXPECT_TRUE(version_number_t{"1.0.0"} < version_number_t{"1.2.3"});
-  EXPECT_TRUE(version_number_t{"1.2.3.9"} < version_number_t{"1.2.4"});
-  EXPECT_TRUE(version_number_t{"1.2.9"} < version_number_t{"1.2.10"});
-  EXPECT_TRUE(version_number_t{"1.2.3"} < version_number_t{"1.2.3 build 1"});
-  EXPECT_TRUE(version_number_t{"1.2.3 build 9"} < version_number_t{"1.2.3 build 10"});
-  EXPECT_TRUE(version_number_t{"9.9.9"} < version_number_t{"11"});
+  EXPECT_TRUE(version_number_t{"1.0.0"}           < version_number_t{"1.2.3"});
+  EXPECT_TRUE(version_number_t{"1.2.3.9"}         < version_number_t{"1.2.4"});
+  EXPECT_TRUE(version_number_t{"1.2.9"}           < version_number_t{"1.2.10"});
+  EXPECT_TRUE(version_number_t{"1.2.3"}           < version_number_t{"1.2.3 build 1"});
+  EXPECT_TRUE(version_number_t{"1.2.3 build 9"}   < version_number_t{"1.2.3 build 10"});
+  EXPECT_TRUE(version_number_t{"9.9.9"}           < version_number_t{"11"});
 }
 
 TEST(VersionNumberT, Stringification) {
-  EXPECT_EQ(std::string{"<invalid>"}, version_number_t{}.to_string());
-  EXPECT_EQ(std::string{"<invalid>"}, version_number_t{""}.to_string());
-  EXPECT_EQ(std::string{"1.2.3"}, version_number_t{"1.2.3"}.to_string());
-  EXPECT_EQ(std::string{"1.2.3.0"}, version_number_t{"1.2.3.0"}.to_string());
-  EXPECT_EQ(std::string{"1.2.3.0"}, version_number_t{"1.2.3.0 build 0"}.to_string());
-  EXPECT_EQ(std::string{"1.2.3.4"}, version_number_t{"1.2.3.4"}.to_string());
-  EXPECT_EQ(std::string{"1.2.3 build 5"}, version_number_t{"1.2.3 build 5"}.to_string());
-  EXPECT_EQ(std::string{"1.2.3.0 build 5"}, version_number_t{"1.2.3.0 build 5"}.to_string());
+  EXPECT_EQ(std::string{"<invalid>"},       version_number_t{}                                   .to_string());
+  EXPECT_EQ(std::string{"<invalid>"},       version_number_t{""}                                 .to_string());
+  EXPECT_EQ(std::string{"1.2.3"},           version_number_t{"1.2.3"}                            .to_string());
+  EXPECT_EQ(std::string{"1.2.3.0"},         version_number_t{"1.2.3.0"}                          .to_string());
+  EXPECT_EQ(std::string{"1.2.3.0"},         version_number_t{"1.2.3.0 build 0"}                  .to_string());
+  EXPECT_EQ(std::string{"1.2.3.4"},         version_number_t{"1.2.3.4"}                          .to_string());
+  EXPECT_EQ(std::string{"1.2.3 build 5"},   version_number_t{"1.2.3 build 5"}                    .to_string());
+  EXPECT_EQ(std::string{"1.2.3.0 build 5"}, version_number_t{"1.2.3.0 build 5"}                  .to_string());
   EXPECT_EQ(std::string{"1.2.3.0 build 5"}, version_number_t{"mkvmerge v1.2.3.0-build20170422-5"}.to_string());
-  EXPECT_EQ(std::string{"11"}, version_number_t{"11"}.to_string());
-  EXPECT_EQ(std::string{"11 build 1213"}, version_number_t{"11 build 1213"}.to_string());
+  EXPECT_EQ(std::string{"11"},              version_number_t{"11"}                               .to_string());
+  EXPECT_EQ(std::string{"11 build 1213"},   version_number_t{"11 build 1213"}                    .to_string());
 }
 
 }
