5eade0597ad4af5eacea05a23e88fc913b87420e
Moritz Bunkus <moritz@bunkus.org>
2017-10-31 15:38:34 +0100

    AVC, HEVC: only copy NALU to RBSP if necessary

--- src/common/mpeg.cpp
+++ src/common/mpeg.cpp
@@ -20,24 +20,33 @@ namespace mtx { namespace mpeg {
 
 memory_cptr
 nalu_to_rbsp(memory_cptr const &buffer) {
-  int pos, size = buffer->get_size();
-  mm_mem_io_c d(nullptr, size, 100);
-  unsigned char *b = buffer->get_buffer();
+  mm_mem_io_cptr d;
+  auto b = buffer->get_buffer();
 
-  for (pos = 0; pos < size; ++pos) {
+  for (int pos = 0, size = buffer->get_size(); pos < size; ++pos) {
     if (   ((pos + 2) < size)
         && (0 == b[pos])
         && (0 == b[pos + 1])
         && (3 == b[pos + 2])) {
-      d.write_uint8(0);
-      d.write_uint8(0);
+
+      if (!d) {
+        d = std::make_shared<mm_mem_io_c>(nullptr, size, 100);
+        if (pos > 0)
+          d->write(b, pos);
+      }
+
+      d->write_uint8(0);
+      d->write_uint8(0);
       pos += 2;
 
-    } else
-      d.write_uint8(b[pos]);
+    } else if (d)
+      d->write_uint8(b[pos]);
   }
 
-  return std::make_shared<memory_c>(d.get_and_lock_buffer(), d.getFilePointer(), true);
+  if (!d)
+    return buffer;
+
+  return std::make_shared<memory_c>(d->get_and_lock_buffer(), d->getFilePointer(), true);
 }
 
 memory_cptr
