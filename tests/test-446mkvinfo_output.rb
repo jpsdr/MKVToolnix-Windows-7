2ec2f1b723d3196da0bd90d72b72ecdc4fc03494
Moritz Bunkus <moritz@bunkus.org>
2017-12-30 21:34:05 +0100

    tests: expand mkvinfo output test

--- tests/test-446mkvinfo_output.rb
+++ tests/test-446mkvinfo_output.rb
@@ -3,7 +3,9 @@
 # T_446mkvinfo_output
 describe "mkvinfo / output of various modes"
 
-test_info "data/mkv/complex.mkv"
-test_info "data/mkv/complex.mkv", :args => "-v -v"
-test_info "data/mkv/complex.mkv", :args => "-v -v -z"
-test_info "data/mkv/complex.mkv", :args => "-s -t"
+%w{complex.mkv attachments.mkv subbed.mkv vobsubs.mks chapters-uk-and-gb.mks chapters-with-ebmlvoid.mkv}.each do |file|
+  test_info "data/mkv/#{file}"
+  test_info "data/mkv/#{file}", :args => "-v -v"
+  test_info "data/mkv/#{file}", :args => "-v -v -z"
+  test_info "data/mkv/#{file}", :args => "-s -t"
+end
