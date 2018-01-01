b0fa335b66cc74982d229c3adc6fb5845c230cb7
Moritz Bunkus <moritz@bunkus.org>
2018-01-01 14:31:51 +0100

    common: add Markdown-to-HTML conversion wrapper around cmark

---
+++ src/common/markdown.cpp
@@ -0,0 +1,37 @@
+/*
+   mkvmerge -- utility for splicing together matroska files
+   from component media subtypes
+
+   Distributed under the GPL v2
+   see the file COPYING for details
+   or visit http://www.gnu.org/copyleft/gpl.html
+
+   Written by Moritz Bunkus <moritz@bunkus.org>.
+*/
+
+#include "common/common_pch.h"
+
+#if defined(HAVE_CMARK)
+
+# include <cmark.h>
+
+# include "common/markdown.h"
+
+namespace mtx { namespace markdown {
+
+std::string
+to_html(std::string const &markdown_text,
+        int options) {
+  auto html = cmark_markdown_to_html(markdown_text.c_str(), markdown_text.length(), options);
+  if (!html)
+    return {};
+
+  auto html_str = std::string{html};
+  free(html);
+
+  return html_str;
+}
+
+}}
+
+#endif  // defined(HAVE_CMARK)
---
+++ src/common/markdown.h
@@ -0,0 +1,24 @@
+/*
+   mkvmerge -- utility for splicing together matroska files
+   from component media subtypes
+
+   Distributed under the GPL v2
+   see the file COPYING for details
+   or visit http://www.gnu.org/copyleft/gpl.html
+
+   Written by Moritz Bunkus <moritz@bunkus.org>.
+*/
+
+#pragma once
+
+#include "common/common_pch.h"
+
+#if defined(HAVE_CMARK)
+
+namespace mtx { namespace markdown {
+
+std::string to_html(std::string const &markdown_text, int options = 0);
+
+}}
+
+#endif  // defined(HAVE_CMARK)
