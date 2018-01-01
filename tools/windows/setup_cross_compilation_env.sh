c722e906931b1df613a30ce073f29915915c81c1
Moritz Bunkus <moritz@bunkus.org>
2018-01-01 16:32:13 +0100

    MXE setup: use the gcc7 plugin

--- tools/windows/setup_cross_compilation_env.sh
+++ tools/windows/setup_cross_compilation_env.sh
@@ -47,7 +47,7 @@ function update_mingw_cross_env {
   cd ${INSTALL_DIR}
   cat > settings.mk <<EOF
 MXE_TARGETS = ${HOST}
-MXE_PLUGIN_DIRS += plugins/gcc6
+MXE_PLUGIN_DIRS += plugins/gcc7
 JOBS = ${PARALLEL}
 
 MKVTOOLNIX_DEPENDENCIES=gettext libiconv zlib boost file flac lzo ogg pthreads vorbis cmark
