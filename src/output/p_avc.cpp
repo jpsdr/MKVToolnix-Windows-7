cc1d815e1be35c9d49dc4e1f39ca2e0b645dedad
mkver <andreas.rheinhardt@googlemail.com>
2018-05-12 10:39:13 +0000

    AVC/h.264 (framed): Remove empty NAL units
    Inspired by FFmpeg ticket 7200.

--- src/output/p_avc.cpp
+++ src/output/p_avc.cpp
@@ -233,7 +233,8 @@ avc_video_packetizer_c::process_nalus(memory_c &data)
 
     auto const nalu_type = ptr[idx + m_nalu_size_len_dst] & 0x1f;
 
-    if (mtx::included_in(nalu_type, NALU_TYPE_FILLER_DATA, NALU_TYPE_ACCESS_UNIT)) {
+    if (   (static_cast<int>(nalu_size) == m_nalu_size_len_dst) // empty NALU?
+        || mtx::included_in(nalu_type, NALU_TYPE_FILLER_DATA, NALU_TYPE_ACCESS_UNIT)) {
       memory_c::splice(data, idx, nalu_size);
       total_size -= nalu_size;
       ptr         = data.get_buffer();
@@ -265,4 +266,8 @@ avc_video_packetizer_c::process_nalus(memory_c &data)
 
     idx += nalu_size;
   }
+
+  // empty NALU at the end?
+  if (total_size == (idx + m_nalu_size_len_dst))
+    data.set_size(idx);
 }
