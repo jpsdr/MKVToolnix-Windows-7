967c66e764b08f1af53438c3e3cd1a4866cfce2d
Moritz Bunkus <moritz@bunkus.org>
2018-10-07 20:18:13 +0200

    tests: intentional update due to AV1 changes

--- tests/test-638av1_ivf_webm.rb
+++ tests/test-638av1_ivf_webm.rb
@@ -1,12 +1,12 @@
 #!/usr/bin/ruby -w
 
 # T_638av1_ivf_webm
-describe "mkvmerge / reading AV1 from IVF, WebM; writing Matroska, WebM"
+describe "mkvmerge / reading AV1 from IVF, WebM, OBU; writing Matroska, WebM"
 
 base = "data/av1/av1."
 
-test_merge "#{base}ivf",  :args => "--engage enable_av1"
-test_merge "#{base}webm", :args => "--engage enable_av1"
-
-test_merge "#{base}ivf",  :args => "--engage enable_av1 --webm"
-test_merge "#{base}webm", :args => "--engage enable_av1 --webm"
+[ "", "--webm" ].each do |args|
+  [ "ivf", "webm", "obu" ].each do |ext|
+    test_merge "#{base}#{ext}", :args => args
+  end
+end
