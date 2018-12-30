7698d43a5508e35c588828d9f7a38cdbd230d220
Moritz Bunkus <moritz@bunkus.org>
2018-12-30 18:35:33 +0100

    add_icons.rb: support for adding scalable versions

--- tools/development/add_icons.rb
+++ tools/development/add_icons.rb
@@ -7,6 +7,15 @@ require "fileutils"
 $base_dir = File.absolute_path(File.dirname(__FILE__) + "/../..")
 
 ARGV.each do |file_name|
+  if %r{/scalable/}.match(file_name)
+    target = "#{$base_dir}/share/icons/scalable/" + file_name.gsub(/.*\//, '')
+
+    puts "#{file_name} → #{target}"
+    FileUtils.cp file_name, target
+
+    next
+  end
+
   size = FastImage.size file_name
   if !size
     puts "Warning: couldn't determine size for #{file_name}"
