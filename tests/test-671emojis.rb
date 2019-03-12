66a2cebc82eb40c1515b6529a1512d150855a7a2
Moritz Bunkus <moritz@bunkus.org>
2019-03-12 20:28:47 +0100

    tests: add test case for Emojis

---
+++ tests/test-671emojis.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/ruby -w
+# coding: utf-8
+
+# T_671emojis
+describe "mkvmerge / Emojis"
+
+emojis = "😀😁😁😂😂😃😃😃😄😄😄😅😅😅😆😆😆😉😊😊😋😋😎😎😎😍😍😍😘😘😗😗😙😙😚😚😘😘😘😍😍😍"
+
+test_merge "data/subtitles/srt/ven.srt", :args => "--title #{emojis}", :keep_tmp => true
+test "identification" do
+  identify_json(tmp)["container"]["properties"]["title"] == emojis
+end
