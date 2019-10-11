2a83672513e46ccbed6b53194a12f99cb8b5231e
Moritz Bunkus <moritz@bunkus.org>
2019-10-11 21:41:11 +0200

    AppImage: use "z" instead of "revision" in git file name
    Allows sorting as intended (git revisions higher than releases) even
    when simply sorting alphabetically.

--- packaging/appimage/build.sh
+++ packaging/appimage/build.sh
@@ -105,7 +105,7 @@ if [[ ( -d .git ) && ( $RELEASE_VERSION == 0 ) ]]; then
   fi
   NUM=${VERSION%-*}
   NUM=${NUM##*-}
-  VERSION="${VERSION%%-*}-revision-$(printf '%03d' ${NUM})-${VERSION##*-}"
+  VERSION="${VERSION%%-*}-z$(printf '%03d' ${NUM})-${VERSION##*-}"
 else
   VERSION="$(perl -ne 'next unless m/^AC_INIT/; s{.*?,\[}{}; s{\].*}{}; print; exit' ${TOP_DIR}/configure.ac)"
 fi
