dc6acc66f9d75464b5fc475b2aa37598ecca96e0
Moritz Bunkus <moritz@bunkus.org>
2019-11-09 10:48:59 +0100

    GUI: mux: "display dimensions" activated instead of "aspect ratio" by default

--- src/mkvtoolnix-gui/merge/track.h
+++ src/mkvtoolnix-gui/merge/track.h
@@ -28,7 +28,7 @@ public:
   TrackType m_type{TrackType::Audio};
   int64_t m_id{-1};
 
-  bool m_muxThis{true}, m_setAspectRatio{true}, m_defaultTrackFlagWasSet{}, m_defaultTrackFlagWasPresent{}, m_forcedTrackFlagWasSet{}, m_aacSbrWasDetected{}, m_nameWasPresent{}, m_fixBitstreamTimingInfo{}, m_reduceAudioToCore{};
+  bool m_muxThis{true}, m_setAspectRatio{}, m_defaultTrackFlagWasSet{}, m_defaultTrackFlagWasPresent{}, m_forcedTrackFlagWasSet{}, m_aacSbrWasDetected{}, m_nameWasPresent{}, m_fixBitstreamTimingInfo{}, m_reduceAudioToCore{};
   bool m_removeDialogNormalizationGain{};
   QString m_name, m_codec, m_language, m_tags, m_delay, m_stretchBy, m_defaultDuration, m_timestamps, m_aspectRatio, m_displayWidth, m_displayHeight, m_cropping, m_characterSet, m_additionalOptions;
   unsigned int m_defaultTrackFlag{}, m_forcedTrackFlag{}, m_stereoscopy{}, m_naluSizeLength{}, m_cues{}, m_aacIsSBR{};
