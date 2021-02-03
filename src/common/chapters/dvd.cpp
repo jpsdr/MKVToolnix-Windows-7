56ea93ab4103d6f7d0ff806a5b2d257becea0ef6
Tom Yan <tom.ty89@gmail.com>
2021-02-03 20:49:12 +0800

    chapters/dvd: fix timestamp derivation
    The timecodes should be interpreted as (integral) number of frames,
    which should be what is counted instead. NTSC-adjustment should be
    applied when we derive timestamp from the last count.
    
    Timestamps are proven to be much more faithful to what is intended
    with this approach.

--- src/common/chapters/dvd.cpp
+++ src/common/chapters/dvd.cpp
@@ -67,9 +67,10 @@ parse_dvd(std::string const &file_name) {
     titles_and_timestamps.emplace_back();
 
     auto &timestamps       = titles_and_timestamps.back();
-    auto overall_timestamp = timestamp_c::ns(0);
     auto ttn               = vmg->tt_srpt->title[title].vts_ttn;
     auto vts_ptt_srpt      = vts->vts_ptt_srpt;
+    auto overall_frames    = 0u;
+    auto fps               = 0u; // This should be consistent as DVDs are either NTSC or PAL
 
     for (auto chapter = 0; chapter < vmg->tt_srpt->title[title].nr_of_ptts - 1; chapter++) {
       auto pgc_id        = vts_ptt_srpt->title[ttn - 1].ptt[chapter].pgcn;
@@ -80,25 +81,26 @@ parse_dvd(std::string const &file_name) {
       pgn                = vts_ptt_srpt->title[ttn - 1].ptt[chapter + 1].pgn;
       cur_pgc            = vts->vts_pgcit->pgci_srp[pgc_id - 1].pgc;
       auto end_cell      = cur_pgc->program_map[pgn - 1] - 2;
-      auto cur_timestamp = timestamp_c::ns(0);
+      auto cur_frames    = 0u;
 
       for (auto cur_cell = start_cell; cur_cell <= end_cell; cur_cell++) {
         auto dt        = &cur_pgc->cell_playback[cur_cell].playback_time;
-        auto fps       = ((dt->frame_u & 0xc0) >> 6) == 1 ? 25.00 : 29.97;
-        auto hour      = ((dt->hour    & 0xf0) >> 4) * 10ull + (dt->hour    & 0x0f);
-        auto minute    = ((dt->minute  & 0xf0) >> 4) * 10ull + (dt->minute  & 0x0f);
-        auto second    = ((dt->second  & 0xf0) >> 4) * 10ull + (dt->second  & 0x0f);
-        auto frames    = ((dt->frame_u & 0x30) >> 4) * 10ull + (dt->frame_u & 0x0f);
-
-        cur_timestamp += timestamp_c::ms((hour * 60 * 60 * 1000) + (minute * 60 * 1000) + (second * 1000) + (frames * 1000.0 / fps));
+        auto hour      = ((dt->hour    & 0xf0) >> 4) * 10 + (dt->hour    & 0x0f);
+        auto minute    = ((dt->minute  & 0xf0) >> 4) * 10 + (dt->minute  & 0x0f);
+        auto second    = ((dt->second  & 0xf0) >> 4) * 10 + (dt->second  & 0x0f);
+        fps            = ((dt->frame_u & 0xc0) >> 6) == 1 ? 25 : 30; // by definition
+        cur_frames    += ((hour * 60 * 60) + minute * 60 + second) * fps;
+        cur_frames    += ((dt->frame_u & 0x30) >> 4) * 10 + (dt->frame_u & 0x0f);
       }
 
-      timestamps.emplace_back(overall_timestamp);
+      auto factor = fps == 30 ? 1001ull : 1000ull;
+      timestamps.emplace_back(timestamp_c::ms(factor * overall_frames / fps));
 
-      overall_timestamp += cur_timestamp;
+      overall_frames += cur_frames;
     }
 
-    timestamps.emplace_back(overall_timestamp);
+    auto factor = fps == 30 ? 1001ull : 1000ull;
+    timestamps.emplace_back(timestamp_c::ms(factor * overall_frames / fps));
   }
 
   return titles_and_timestamps;
