efe74a6ceae0c638a7c1cbd54169ce1f623e80c5
Moritz Bunkus <mo@bunkus.online>
2021-02-19 23:00:43 +0100

    tests: make 717 independent of where the test files reside in the file system

--- tests/test-717bluray_identification.rb
+++ tests/test-717bluray_identification.rb
@@ -5,5 +5,10 @@ describe "mkvmerge / Blu-ray directory structure identification"
 
 file = "data/bluray/ganster_squad/BDMV/PLAYLIST/00100.mpls"
 
-test_identify file
-test_merge    file
+test "identification" do
+  json = identify_json(file)
+  json["container"]["properties"]["playlist_file"] = json["container"]["properties"]["playlist_file"].map { |pl_file| pl_file.gsub(%r{.*/data/}, 'data/') }
+  json.to_json.md5
+end
+
+test_merge file
