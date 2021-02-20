2dda7860a4256d9e11fbdbb71646bdfc559a3bc8
Moritz Bunkus <mo@bunkus.online>
2021-02-20 22:30:37 +0100

    info CLI parser: don't use #defines

--- src/info/info_cli_parser.cpp
+++ src/info/info_cli_parser.cpp
@@ -26,25 +26,23 @@ info_cli_parser_c::info_cli_parser_c(const std::vector<std::string> &args)
   verbose = 0;
 }
 
-#define OPT(spec, func, description) add_option(spec, std::bind(&info_cli_parser_c::func, this), description)
-
 void
 info_cli_parser_c::init_parser() {
   add_information(YT("mkvinfo [options] <inname>"));
 
   add_section_header(YT("Options"));
 
-  OPT("a|all",           set_show_all_elements,   YT("Show all sub-elements (including cues & seek heads entries) and don't stop at the first cluster."));
-  OPT("c|checksum",      set_checksum,            YT("Calculate and display checksums of frame contents."));
-  OPT("C|check-mode",    set_check_mode,          YT("Calculate and display checksums and use verbosity level 4."));
-  OPT("o|continue",      set_continue_at_cluster, YT("Don't stop processing at the first cluster."));
-  OPT("P|positions",     set_dec_positions,       YT("Show the position of each element in decimal."));
-  OPT("p|hex-positions", set_hex_positions,       YT("Show the position of each element in hexadecimal."));
-  OPT("s|summary",       set_summary,             YT("Only show summaries of the contents, not each element."));
-  OPT("t|track-info",    set_track_info,          YT("Show statistics for each track in verbose mode."));
-  OPT("x|hexdump",       set_hexdump,             YT("Show the first 16 bytes of each frame as a hex dump."));
-  OPT("X|full-hexdump",  set_full_hexdump,        YT("Show all bytes of each frame as a hex dump."));
-  OPT("z|size",          set_size,                YT("Show the size of each element including its header."));
+  add_option("a|all",           std::bind(&info_cli_parser_c::set_show_all_elements,   this), YT("Show all sub-elements (including cues & seek heads entries) and don't stop at the first cluster."));
+  add_option("c|checksum",      std::bind(&info_cli_parser_c::set_checksum,            this), YT("Calculate and display checksums of frame contents."));
+  add_option("C|check-mode",    std::bind(&info_cli_parser_c::set_check_mode,          this), YT("Calculate and display checksums and use verbosity level 4."));
+  add_option("o|continue",      std::bind(&info_cli_parser_c::set_continue_at_cluster, this), YT("Don't stop processing at the first cluster."));
+  add_option("P|positions",     std::bind(&info_cli_parser_c::set_dec_positions,       this), YT("Show the position of each element in decimal."));
+  add_option("p|hex-positions", std::bind(&info_cli_parser_c::set_hex_positions,       this), YT("Show the position of each element in hexadecimal."));
+  add_option("s|summary",       std::bind(&info_cli_parser_c::set_summary,             this), YT("Only show summaries of the contents, not each element."));
+  add_option("t|track-info",    std::bind(&info_cli_parser_c::set_track_info,          this), YT("Show statistics for each track in verbose mode."));
+  add_option("x|hexdump",       std::bind(&info_cli_parser_c::set_hexdump,             this), YT("Show the first 16 bytes of each frame as a hex dump."));
+  add_option("X|full-hexdump",  std::bind(&info_cli_parser_c::set_full_hexdump,        this), YT("Show all bytes of each frame as a hex dump."));
+  add_option("z|size",          std::bind(&info_cli_parser_c::set_size,                this), YT("Show the size of each element including its header."));
 
   add_common_options();
 
