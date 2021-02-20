db274b4e8aad11074da812a80cde3e3a038ae6f2
Moritz Bunkus <mo@bunkus.online>
2021-02-20 17:24:05 +0100

    codecs: use constexpr instead of #define

--- src/common/codec.h
+++ src/common/codec.h
@@ -23,72 +23,72 @@
 
 // see http://www.matroska.org/technical/specs/codecid/index.html
 
-#define MKV_A_AAC          "A_AAC"
-#define MKV_A_AAC_2LC      "A_AAC/MPEG2/LC"
-#define MKV_A_AAC_2MAIN    "A_AAC/MPEG2/MAIN"
-#define MKV_A_AAC_2SBR     "A_AAC/MPEG2/LC/SBR"
-#define MKV_A_AAC_2SSR     "A_AAC/MPEG2/SSR"
-#define MKV_A_AAC_4LC      "A_AAC/MPEG4/LC"
-#define MKV_A_AAC_4LTP     "A_AAC/MPEG4/LTP"
-#define MKV_A_AAC_4MAIN    "A_AAC/MPEG4/MAIN"
-#define MKV_A_AAC_4SBR     "A_AAC/MPEG4/LC/SBR"
-#define MKV_A_AAC_4SSR     "A_AAC/MPEG4/SSR"
-#define MKV_A_AC3          "A_AC3"
-#define MKV_A_ACM          "A_MS/ACM"
-#define MKV_A_ALAC         "A_ALAC"
-#define MKV_A_DTS          "A_DTS"
-#define MKV_A_EAC3         "A_EAC3"
-#define MKV_A_FLAC         "A_FLAC"
-#define MKV_A_MLP          "A_MLP"
-#define MKV_A_MP2          "A_MPEG/L2"
-#define MKV_A_MP3          "A_MPEG/L3"
-#define MKV_A_OPUS         "A_OPUS"
-#define MKV_A_PCM          "A_PCM/INT/LIT"
-#define MKV_A_PCM_BE       "A_PCM/INT/BIG"
-#define MKV_A_PCM_FLOAT    "A_PCM/FLOAT/IEEE"
-#define MKV_A_QDMC         "A_QUICKTIME/QDMC"
-#define MKV_A_QDMC2        "A_QUICKTIME/QDM2"
-#define MKV_A_QUICKTIME    "A_QUICKTIME"
-#define MKV_A_TRUEHD       "A_TRUEHD"
-#define MKV_A_TTA          "A_TTA1"
-#define MKV_A_VORBIS       "A_VORBIS"
-#define MKV_A_WAVPACK4     "A_WAVPACK4"
+constexpr auto MKV_A_AAC          = "A_AAC";
+constexpr auto MKV_A_AAC_2LC      = "A_AAC/MPEG2/LC";
+constexpr auto MKV_A_AAC_2MAIN    = "A_AAC/MPEG2/MAIN";
+constexpr auto MKV_A_AAC_2SBR     = "A_AAC/MPEG2/LC/SBR";
+constexpr auto MKV_A_AAC_2SSR     = "A_AAC/MPEG2/SSR";
+constexpr auto MKV_A_AAC_4LC      = "A_AAC/MPEG4/LC";
+constexpr auto MKV_A_AAC_4LTP     = "A_AAC/MPEG4/LTP";
+constexpr auto MKV_A_AAC_4MAIN    = "A_AAC/MPEG4/MAIN";
+constexpr auto MKV_A_AAC_4SBR     = "A_AAC/MPEG4/LC/SBR";
+constexpr auto MKV_A_AAC_4SSR     = "A_AAC/MPEG4/SSR";
+constexpr auto MKV_A_AC3          = "A_AC3";
+constexpr auto MKV_A_ACM          = "A_MS/ACM";
+constexpr auto MKV_A_ALAC         = "A_ALAC";
+constexpr auto MKV_A_DTS          = "A_DTS";
+constexpr auto MKV_A_EAC3         = "A_EAC3";
+constexpr auto MKV_A_FLAC         = "A_FLAC";
+constexpr auto MKV_A_MLP          = "A_MLP";
+constexpr auto MKV_A_MP2          = "A_MPEG/L2";
+constexpr auto MKV_A_MP3          = "A_MPEG/L3";
+constexpr auto MKV_A_OPUS         = "A_OPUS";
+constexpr auto MKV_A_PCM          = "A_PCM/INT/LIT";
+constexpr auto MKV_A_PCM_BE       = "A_PCM/INT/BIG";
+constexpr auto MKV_A_PCM_FLOAT    = "A_PCM/FLOAT/IEEE";
+constexpr auto MKV_A_QDMC         = "A_QUICKTIME/QDMC";
+constexpr auto MKV_A_QDMC2        = "A_QUICKTIME/QDM2";
+constexpr auto MKV_A_QUICKTIME    = "A_QUICKTIME";
+constexpr auto MKV_A_TRUEHD       = "A_TRUEHD";
+constexpr auto MKV_A_TTA          = "A_TTA1";
+constexpr auto MKV_A_VORBIS       = "A_VORBIS";
+constexpr auto MKV_A_WAVPACK4     = "A_WAVPACK4";
 
-#define MKV_V_AV1          "V_AV1"
-#define MKV_V_DIRAC        "V_DIRAC"
-#define MKV_V_MPEG1        "V_MPEG1"
-#define MKV_V_MPEG2        "V_MPEG2"
-#define MKV_V_MPEG4_AP     "V_MPEG4/ISO/AP"
-#define MKV_V_MPEG4_ASP    "V_MPEG4/ISO/ASP"
-#define MKV_V_MPEG4_AVC    "V_MPEG4/ISO/AVC"
-#define MKV_V_MPEG4_SP     "V_MPEG4/ISO/SP"
-#define MKV_V_MPEGH_HEVC   "V_MPEGH/ISO/HEVC"
-#define MKV_V_MSCOMP       "V_MS/VFW/FOURCC"
-#define MKV_V_PRORES       "V_PRORES"
-#define MKV_V_QUICKTIME    "V_QUICKTIME"
-#define MKV_V_REALV1       "V_REAL/RV10"
-#define MKV_V_REALV2       "V_REAL/RV20"
-#define MKV_V_REALV3       "V_REAL/RV30"
-#define MKV_V_REALV4       "V_REAL/RV40"
-#define MKV_V_THEORA       "V_THEORA"
-#define MKV_V_UNCOMPRESSED "V_UNCOMPRESSED"
-#define MKV_V_VP8          "V_VP8"
-#define MKV_V_VP9          "V_VP9"
+constexpr auto MKV_V_AV1          = "V_AV1";
+constexpr auto MKV_V_DIRAC        = "V_DIRAC";
+constexpr auto MKV_V_MPEG1        = "V_MPEG1";
+constexpr auto MKV_V_MPEG2        = "V_MPEG2";
+constexpr auto MKV_V_MPEG4_AP     = "V_MPEG4/ISO/AP";
+constexpr auto MKV_V_MPEG4_ASP    = "V_MPEG4/ISO/ASP";
+constexpr auto MKV_V_MPEG4_AVC    = "V_MPEG4/ISO/AVC";
+constexpr auto MKV_V_MPEG4_SP     = "V_MPEG4/ISO/SP";
+constexpr auto MKV_V_MPEGH_HEVC   = "V_MPEGH/ISO/HEVC";
+constexpr auto MKV_V_MSCOMP       = "V_MS/VFW/FOURCC";
+constexpr auto MKV_V_PRORES       = "V_PRORES";
+constexpr auto MKV_V_QUICKTIME    = "V_QUICKTIME";
+constexpr auto MKV_V_REALV1       = "V_REAL/RV10";
+constexpr auto MKV_V_REALV2       = "V_REAL/RV20";
+constexpr auto MKV_V_REALV3       = "V_REAL/RV30";
+constexpr auto MKV_V_REALV4       = "V_REAL/RV40";
+constexpr auto MKV_V_THEORA       = "V_THEORA";
+constexpr auto MKV_V_UNCOMPRESSED = "V_UNCOMPRESSED";
+constexpr auto MKV_V_VP8          = "V_VP8";
+constexpr auto MKV_V_VP9          = "V_VP9";
 
-#define MKV_S_DVBSUB       "S_DVBSUB"
-#define MKV_S_HDMV_PGS     "S_HDMV/PGS"
-#define MKV_S_HDMV_TEXTST  "S_HDMV/TEXTST"
-#define MKV_S_KATE         "S_KATE"
-#define MKV_S_TEXTASCII    "S_TEXT/ASCII"
-#define MKV_S_TEXTASS      "S_TEXT/ASS"
-#define MKV_S_TEXTSSA      "S_TEXT/SSA"
-#define MKV_S_TEXTUSF      "S_TEXT/USF"
-#define MKV_S_TEXTUTF8     "S_TEXT/UTF8"
-#define MKV_S_TEXTWEBVTT   "S_TEXT/WEBVTT"
-#define MKV_S_VOBSUB       "S_VOBSUB"
-#define MKV_S_VOBSUBZLIB   "S_VOBSUB/ZLIB"
+constexpr auto MKV_S_DVBSUB       = "S_DVBSUB";
+constexpr auto MKV_S_HDMV_PGS     = "S_HDMV/PGS";
+constexpr auto MKV_S_HDMV_TEXTST  = "S_HDMV/TEXTST";
+constexpr auto MKV_S_KATE         = "S_KATE";
+constexpr auto MKV_S_TEXTASCII    = "S_TEXT/ASCII";
+constexpr auto MKV_S_TEXTASS      = "S_TEXT/ASS";
+constexpr auto MKV_S_TEXTSSA      = "S_TEXT/SSA";
+constexpr auto MKV_S_TEXTUSF      = "S_TEXT/USF";
+constexpr auto MKV_S_TEXTUTF8     = "S_TEXT/UTF8";
+constexpr auto MKV_S_TEXTWEBVTT   = "S_TEXT/WEBVTT";
+constexpr auto MKV_S_VOBSUB       = "S_VOBSUB";
+constexpr auto MKV_S_VOBSUBZLIB   = "S_VOBSUB/ZLIB";
 
-#define MKV_B_VOBBTN       "B_VOBBTN"
+constexpr auto MKV_B_VOBBTN       = "B_VOBBTN";
 
 class codec_private_c;
 class codec_c {
