43bf6ac0937f4effc368a432470780b9bd1c93d3
Moritz Bunkus <mo@bunkus.online>
2021-03-15 13:54:49 +0100

    src/common/track_statistics.cpp: remove "using namespace libmatroska"

--- src/common/track_statistics.cpp
+++ src/common/track_statistics.cpp
@@ -16,10 +16,8 @@
 #include "common/tags/tags.h"
 #include "common/track_statistics.h"
 
-using namespace libmatroska;
-
 void
-track_statistics_c::create_tags(KaxTags &tags,
+track_statistics_c::create_tags(libmatroska::KaxTags &tags,
                                 std::string const &writing_app,
                                 std::optional<mtx::date_time::point_t> const &writing_date)
   const {
@@ -27,12 +25,12 @@ track_statistics_c::create_tags(KaxTags &tags,
   auto duration = get_duration();
   auto names    = std::vector<std::string>{ "BPS"s, "DURATION"s, "NUMBER_OF_FRAMES"s, "NUMBER_OF_BYTES"s };
 
-  mtx::tags::remove_simple_tags_for<KaxTagTrackUID>(tags, m_track_uid, "BPS");
-  mtx::tags::remove_simple_tags_for<KaxTagTrackUID>(tags, m_track_uid, "DURATION");
-  mtx::tags::remove_simple_tags_for<KaxTagTrackUID>(tags, m_track_uid, "NUMBER_OF_FRAMES");
-  mtx::tags::remove_simple_tags_for<KaxTagTrackUID>(tags, m_track_uid, "NUMBER_OF_BYTES");
+  mtx::tags::remove_simple_tags_for<libmatroska::KaxTagTrackUID>(tags, m_track_uid, "BPS");
+  mtx::tags::remove_simple_tags_for<libmatroska::KaxTagTrackUID>(tags, m_track_uid, "DURATION");
+  mtx::tags::remove_simple_tags_for<libmatroska::KaxTagTrackUID>(tags, m_track_uid, "NUMBER_OF_FRAMES");
+  mtx::tags::remove_simple_tags_for<libmatroska::KaxTagTrackUID>(tags, m_track_uid, "NUMBER_OF_BYTES");
 
-  auto tag = mtx::tags::find_tag_for<KaxTagTrackUID>(tags, m_track_uid, mtx::tags::Movie, true, "MOVIE");
+  auto tag = mtx::tags::find_tag_for<libmatroska::KaxTagTrackUID>(tags, m_track_uid, mtx::tags::Movie, true, "MOVIE");
 
   mtx::tags::set_simple(*tag, "BPS",              fmt::to_string(bps ? *bps : 0));
   mtx::tags::set_simple(*tag, "DURATION",         mtx::string::format_timestamp(duration ? *duration : 0));
