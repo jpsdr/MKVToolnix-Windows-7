935b90fe5dba16d0b0ad9a5777af8a76bbf02cb6
Moritz Bunkus <mo@bunkus.online>
2021-07-06 20:58:04 +0200

    tests: don't rely on implicit subtitle charset detection

--- tests/test-348srt_negative_timecodes2.rb
+++ tests/test-348srt_negative_timecodes2.rb
@@ -3,4 +3,4 @@
 # T_348srt_negative_timecodes2
 describe "mkvmerge / SRT subitles with negative timecodes"
 
-test_merge "data/subtitles/srt/negative_timecodes2.srt", :exit_code => 1
+test_merge "--subtitle-charset -1:ISO-8859-15 data/subtitles/srt/negative_timecodes2.srt", :exit_code => 1
