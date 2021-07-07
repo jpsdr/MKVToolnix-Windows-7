31ac43f0decd06efdf2aa5107ccb7d534ce2bc8b
Moritz Bunkus <mo@bunkus.online>
2021-07-07 14:50:06 +0200

    tests: 628: specify subtitle character set

--- tests/test-628srt_no_decimal_places.rb
+++ tests/test-628srt_no_decimal_places.rb
@@ -2,4 +2,4 @@
 
 # T_628srt_no_decimal_places
 describe "mkvmerge / SRT with timestamps without decimal places"
-test_merge "data/subtitles/srt/timestamps_without_decimal_places.srt", :exit_code => :warning
+test_merge "--subtitle-charset 0:CP1252 data/subtitles/srt/timestamps_without_decimal_places.srt", :exit_code => :warning
