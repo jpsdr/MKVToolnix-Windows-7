0ac5cb9babfa10f5d520bc46ccbf279706552870
Moritz Bunkus <mo@bunkus.online>
2021-07-07 09:18:25 +0200

    tests: skip default-charset-dependant sub-test on Windows

--- tests/test-406ogm_chapters_ansi_encoded.rb
+++ tests/test-406ogm_chapters_ansi_encoded.rb
@@ -5,5 +5,5 @@ file = "data/ogg/with_chapters_ansi_encoded.ogm"
 describe "mkvmerge / identify and merge OGMs with comments encoded in ANSI"
 
 test_identify file
-test_merge file, :exit_code => :warning
+test_merge file, :exit_code => :warning, :skip_if => $is_windows
 test_merge file, :args => "--chapter-charset MS-ANSI"
