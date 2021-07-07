3e8c1bda2c89f163d7433c5d71c8d3316935424a
Moritz Bunkus <mo@bunkus.online>
2021-07-07 15:32:05 +0200

    tests: 717: fix running on Windows

--- tests/test-717bluray_identification.rb
+++ tests/test-717bluray_identification.rb
@@ -7,7 +7,7 @@ file = "data/bluray/ganster_squad/BDMV/PLAYLIST/00100.mpls"
 
 test "identification" do
   json = identify_json(file)
-  json["container"]["properties"]["playlist_file"] = json["container"]["properties"]["playlist_file"].map { |pl_file| pl_file.gsub(%r{.*/data/}, 'data/') }
+  json["container"]["properties"]["playlist_file"] = json["container"]["properties"]["playlist_file"].map { |pl_file| pl_file.gsub(%r{\\}, '/').gsub(%r{.*/data/}, 'data/') }
   json.to_json.md5
 end
 
