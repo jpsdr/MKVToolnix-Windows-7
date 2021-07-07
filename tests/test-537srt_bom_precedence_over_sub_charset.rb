2db55aa25be52499a981542bb675d13802fdcfda
Moritz Bunkus <mo@bunkus.online>
2021-07-07 13:22:22 +0200

    tests: 537: skip sub-test relying on default charset on Windows

--- tests/test-537srt_bom_precedence_over_sub_charset.rb
+++ tests/test-537srt_bom_precedence_over_sub_charset.rb
@@ -6,4 +6,4 @@ describe "mkvmerge / SRT: BOMs have precedence over --sub-charset"
 test_merge "--sub-charset 0:ISO-8859-15 data/subtitles/srt/vde-utf-8-bom.srt"
 test_merge "                            data/subtitles/srt/vde-utf-8-bom.srt"
 test_merge "--sub-charset 0:ISO-8859-15 data/subtitles/srt/vde.srt"
-test_merge "                            data/subtitles/srt/vde.srt", :exit_code => :warning
+test_merge "                            data/subtitles/srt/vde.srt", :exit_code => :warning, :skip_if => $is_windows
