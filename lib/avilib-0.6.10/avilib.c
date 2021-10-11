jpsdr/MKVToolnix-Windows-7

commit 7d32fdf22375471874920e6ec995d0a10aa05fe6

AVI reader, avilib: support for aspect ratio in video properties header
Implements #2993.

lib/avilib-0.6.10/avilib.c
@@ -2667,9 +2667,16 @@ int avi_parse_input_file(avi_t *AVI, int getIndex)
 	 }
 	 i += 8;
       }
+      else if (strncasecmp((char *)(hdrl_data+i),"vprp",4) == 0) {
+        if (   (lasttag == 1)
+            && !AVI->video_properties_valid
+            && ((sizeof(alVIDEO_PROPERTIES) + 8) <= n)) {
+          AVI->video_properties_valid = 1;
+          memcpy(&AVI->video_properties, hdrl_data + i + 8, sizeof(alVIDEO_PROPERTIES));
+        }
+      }
       else if((strncasecmp((char *)(hdrl_data+i),"JUNK",4) == 0) ||
-              (strncasecmp((char *)(hdrl_data+i),"strn",4) == 0) ||
-              (strncasecmp((char *)(hdrl_data+i),"vprp",4) == 0)){
+              (strncasecmp((char *)(hdrl_data+i),"strn",4) == 0)) {
 	 i += 8;
 	 // do not reset lasttag
       } else

lib/avilib-0.6.10/avilib.h
@@ -223,6 +223,21 @@ typedef struct PACKED_STRUCTURE
   uint16_t dw_right;
   uint16_t dw_bottom;
 } alAVISTREAMHEADER;
+
+typedef struct PACKED_STRUCTURE {
+  uint32_t video_format_token;
+  uint32_t video_standard;
+  uint32_t vertical_refresh_rate;
+  uint32_t h_total_in_t;
+  uint32_t v_total_in_lines;
+  uint16_t frame_aspect_ratio_y;
+  uint16_t frame_aspect_ratio_x;
+  uint32_t frame_width_in_pixels;
+  uint32_t frame_height_in_lines;
+  uint32_t fields_per_frame;
+  // VIDEO_FIELD_DESC field_info[fields_per_frame]
+} alVIDEO_PROPERTIES;
+
 #if defined(COMP_MSC)
 #pragma pack(pop)
 #endif
@@ -280,6 +295,8 @@ typedef struct
   alBITMAPINFOHEADER *bitmap_info_header;
   alWAVEFORMATEX *wave_format_ex[AVI_MAX_TRACKS];
   alAVISTREAMHEADER stream_headers[AVI_MAX_TRACKS];
+  alVIDEO_PROPERTIES video_properties;
+  int video_properties_valid;
 
   void*		extradata;
   unsigned long	extradata_size;

src/input/r_avi.cpp
@@ -96,6 +96,8 @@ avi_reader_c::read_headers() {
   m_video_width      = std::abs(AVI_video_width(m_avi));
   m_video_height     = std::abs(AVI_video_height(m_avi));
 
+  handle_video_aspect_ratio();
+
   verify_video_track();
   parse_subtitle_chunks();
 
@@ -234,6 +236,44 @@ avi_reader_c::create_video_packetizer() {
 
   else
     create_standard_video_packetizer();
+
+  if (m_video_display_width)
+    PTZR(m_vptzr)->set_video_display_dimensions(m_video_display_width, m_video_display_height, generic_packetizer_c::ddu_pixels, OPTION_SOURCE_CONTAINER);
+}
+
+void
+avi_reader_c::handle_video_aspect_ratio() {
+  if (!m_avi->video_properties_valid) {
+    mxdebug_if(m_debug_aspect_ratio, fmt::format("handle_video_aspect_ratio: video properties header not present\n"));
+    return;
+  }
+
+  uint64_t x = get_uint16_le(&m_avi->video_properties.frame_aspect_ratio_x),
+           y = get_uint16_le(&m_avi->video_properties.frame_aspect_ratio_y);
+
+  if (!x || !y) {
+    mxdebug_if(m_debug_aspect_ratio, fmt::format("handle_video_aspect_ratio: invalid aspect ratio {0}:{1}, ignoring\n", x, y));
+    return;
+  }
+
+  if (!m_video_width || !m_video_height) {
+    mxdebug_if(m_debug_aspect_ratio, fmt::format("handle_video_aspect_ratio: invalid video dimensions {0}x{1}, ignoring\n", m_video_width, m_video_height));
+    return;
+  }
+
+  auto aspect_ratio = int64_rational_c{x, y};
+
+  if (aspect_ratio >= int64_rational_c{m_video_width, m_video_height}) {
+    m_video_display_width  = boost::rational_cast<int64_t>(aspect_ratio * m_video_height);
+    m_video_display_height = m_video_height;
+
+  } else {
+    m_video_display_width  = m_video_width;
+    m_video_display_height = boost::rational_cast<int64_t>(int64_rational_c{y, x} * m_video_width);
+  }
+
+  mxdebug_if(m_debug_aspect_ratio, fmt::format("handle_video_aspect_ratio: frame aspect ratio {0}:{1} pixel dimensions {2}x{3} display dimensions {4}x{5}\n",
+                                               x, y, m_video_width, m_video_height, m_video_display_width, m_video_display_height));
 }
 
 void
@@ -862,6 +902,9 @@ avi_reader_c::identify_video() {
     info.add(mtx::id::packetizer, mtx::id::mpeg4_p10_es_video);
 
   info.add(mtx::id::pixel_dimensions, fmt::format("{0}x{1}", m_video_width, m_video_height));
+
+  if (m_video_display_width)
+    info.add(mtx::id::display_dimensions, fmt::format("{0}x{1}", m_video_display_width, m_video_display_height));
 
   id_result_track(0, ID_RESULT_TRACK_VIDEO, codec.get_name(fourcc_str), info.get());
 }

src/input/r_avi.h
@@ -65,11 +65,13 @@ private:
   std::vector<avi_subs_demuxer_t> m_subtitle_demuxers;
   double m_fps{1.0};
   unsigned int m_video_frames_read{}, m_max_video_frames{}, m_dropped_video_frames{};
-  unsigned int m_video_width{}, m_video_height{};
+  unsigned int m_video_width{}, m_video_height{}, m_video_display_width{}, m_video_display_height;
   int m_avc_nal_size_size{-1};
 
   uint64_t m_bytes_to_process{}, m_bytes_processed{};
   bool m_video_track_ok{};
+
+  debugging_option_c m_debug_aspect_ratio{"avi|avi_aspect_ratio"};
 
 public:
   virtual ~avi_reader_c();
@@ -94,6 +96,8 @@ protected:
   virtual file_status_e read_video();
   virtual file_status_e read_audio(avi_demuxer_t &demuxer);
   virtual file_status_e read_subtitles(avi_subs_demuxer_t &demuxer);
+
+  virtual void handle_video_aspect_ratio();
 
   virtual generic_packetizer_c *create_aac_packetizer(int aid, avi_demuxer_t &demuxer);
   virtual generic_packetizer_c *create_dts_packetizer(int aid);

tests/test-714avi_video_aspect_ratio.rb
@@ -1,0 +1,8 @@
+#!/usr/bin/ruby -w
+
+# T_714avi_video_aspect_ratio
+file = "data/avi/video_aspect_ratio.avi"
+
+describe "mkvmerge / AVI with video aspect ratio in video properties header"
+test_merge file
+test_identify file

commit 76d0b612879c588f02167b279a2864237b83e733

AVI parser: fix reading & skipping video property header chunks
This one got broken in 7d32fdf22375471874920e6ec995d0a10aa05fe6. The
effect was that following chunks weren't read correctly. If the
following chunks were index chunks, the parts of the file the index
chunks pointed to weren't read either.

Fixes #3209.

lib/avilib-0.6.10/avilib.c
@@ -2674,6 +2674,7 @@ int avi_parse_input_file(avi_t *AVI, int getIndex)
           AVI->video_properties_valid = 1;
           memcpy(&AVI->video_properties, hdrl_data + i + 8, sizeof(alVIDEO_PROPERTIES));
         }
+        i += 8;
       }
       else if((strncasecmp((char *)(hdrl_data+i),"JUNK",4) == 0) ||
               (strncasecmp((char *)(hdrl_data+i),"strn",4) == 0)) {