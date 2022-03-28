d91ee1da26416ebac74a484762835741fdcac373
Moritz Bunkus <mo@bunkus.online>
2022-03-28 17:26:12 +0200

    tests: use source file that's faster to process

--- tests/test-0735normalize_language_ietf_tracks.rb
+++ tests/test-0735normalize_language_ietf_tracks.rb
@@ -3,13 +3,13 @@
 # T_735normalize_language_ietf_tracks
 describe "mkvmerge / normalize language IETF in tracks"
 
-src = "data/avi/v.avi"
+src = "data/subtitles/srt/ven.srt"
 
-test_merge src, :args => "--language 0:fr-fx --language 1:zh-yue --normalize-language-ietf off", :keep_tmp => true
+test_merge src, :args => "--language 0:fr-fx #{src} --language 0:zh-yue --normalize-language-ietf off", :keep_tmp => true
 compare_languages_tracks [ "fre", "fr-FX" ], [ "chi", "zh-yue" ]
 
-test_merge src, :args => "--language 0:fr-fx --language 1:zh-yue", :keep_tmp => true
+test_merge src, :args => "--language 0:fr-fx #{src} --language 0:zh-yue", :keep_tmp => true
 compare_languages_tracks [ "fre", "fr-FR" ], [ "chi", "yue" ]
 
-test_merge src, :args => "--language 0:fr-fx --language 1:yue --normalize-language-ietf extlang", :keep_tmp => true
+test_merge src, :args => "--language 0:fr-fx #{src} --language 0:yue --normalize-language-ietf extlang", :keep_tmp => true
 compare_languages_tracks [ "fre", "fr-FR" ], [ "chi", "zh-yue" ]
