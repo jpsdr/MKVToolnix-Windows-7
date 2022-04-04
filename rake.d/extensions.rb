8c3e827e2a160e32b402f209d667ef9c19b1b6b0
Moritz Bunkus <mo@bunkus.online>
2022-04-04 18:47:23 +0200

    build system: fix OS-based file selection for non-Linux/macOS/Windows systems
    Several files have OS-specific names like "…/windows.cpp", and they
    should only be compiled when currently compiling for that OS. The
    three main OS mentioned in the build system are Linux, macOS and
    Windows. Other Unices like FreeBSD aren't mentioned explicitly and
    should be treated the same as Linux (as the code in those
    Linux-specific files is rather Unix-but-not-macOS-specific).
    
    Fixes #3316.

--- rake.d/extensions.rb
+++ rake.d/extensions.rb
@@ -62,14 +62,14 @@ class Array
       :windows => %w{linux macos unix         x11},
     }
 
-    reject.each do |os, types|
-      next if !$building_for[os]
+    # Treat other OS (e.g. FreeBSD) the same as Linux wrt. which files to compile
+    os    = $building_for.keys.select { |key| $building_for[key] }.first
+    types = reject[os || :linux]
 
-      re = '(?:' + types.join('|') + ')'
-      re = %r{(?:/|^)#{re}[_.]}
+    re    = '(?:' + types.join('|') + ')'
+    re    = %r{(?:/|^)#{re}[_.]}
 
-      self.reject! { |f| re.match f }
-    end
+    self.reject! { |f| re.match f }
 
     return self
   end
