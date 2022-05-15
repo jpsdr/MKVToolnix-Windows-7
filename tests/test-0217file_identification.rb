522c7b00304e0f8ed1e029cd6c7b1091e0a77797
Moritz Bunkus <mo@bunkus.online>
2022-05-15 17:50:36 +0200

    tests: make certain tests insusceptible to identification format version changes

--- tests/test-0217file_identification.rb
+++ tests/test-0217file_identification.rb
@@ -1,39 +1,38 @@
 #!/usr/bin/ruby -w
 
-class T_0217file_identification < Test
-  def description
-    return "mkvmerge / file identification / in(*)"
+# T_0217file_identification
+
+describe "mkvmerge / file identification / in(*)"
+
+test "identification" do
+  checksum = [
+    "data/avi/v.avi",
+    "data/bugs/From_Nero_AVC_Muxer.mp4",
+    "data/mkv/complex.mkv",
+    "data/mkv/vobsubs.mks",
+    "data/mp4/test_2000_inloop.mp4",
+    "data/mp4/test_mp2.mp4",
+    "data/ogg/v.flac.ogg",
+    "data/ogg/v.ogg",
+    "data/rm/rv3.rm",
+    "data/rm/rv4.rm",
+    "data/ac3/misdetected_as_mp2.ac3",
+    "data/ac3/misdetected_as_mpeges.ac3",
+    "data/aac/v.aac",
+    "data/ac3/v.ac3",
+    "data/flac/v.flac",
+    "data/simple/v.mp3",
+    "data/wav/v.wav",
+    "data/subtitles/ssa-ass/fe.ssa",
+    "data/subtitles/srt/vde.srt",
+    "data/vobsub/ally1-short.sub",
+    "data/wp/with-correction.wv",
+    "data/wp/without-correction.wv"
+  ].collect do |file|
+    json = identify_json(file)
+    json.delete("identification_format_version")
+    json.to_json.md5
   end
 
-  def run
-    checksum = [
-      "data/avi/v.avi",
-      "data/bugs/From_Nero_AVC_Muxer.mp4",
-      "data/mkv/complex.mkv",
-      "data/mkv/vobsubs.mks",
-      "data/mp4/test_2000_inloop.mp4",
-      "data/mp4/test_mp2.mp4",
-      "data/ogg/v.flac.ogg",
-      "data/ogg/v.ogg",
-      "data/rm/rv3.rm",
-      "data/rm/rv4.rm",
-      "data/ac3/misdetected_as_mp2.ac3",
-      "data/ac3/misdetected_as_mpeges.ac3",
-      "data/aac/v.aac",
-      "data/ac3/v.ac3",
-      "data/flac/v.flac",
-      "data/simple/v.mp3",
-      "data/wav/v.wav",
-      "data/subtitles/ssa-ass/fe.ssa",
-      "data/subtitles/srt/vde.srt",
-      "data/vobsub/ally1-short.sub",
-      "data/wp/with-correction.wv",
-      "data/wp/without-correction.wv"
-    ].collect do |file|
-      sys "../src/mkvmerge -J #{file} > #{tmp}", 0
-      hash_tmp
-    end
-
-    checksum.join '-'
-  end
+  checksum.join '-'
 end
--- tests/test-0717bluray_identification.rb
+++ tests/test-0717bluray_identification.rb
@@ -8,6 +8,7 @@ file = "data/bluray/ganster_squad/BDMV/PLAYLIST/00100.mpls"
 test "identification" do
   json = identify_json(file)
   json["container"]["properties"]["playlist_file"] = json["container"]["properties"]["playlist_file"].map { |pl_file| pl_file.gsub(%r{\\}, '/').gsub(%r{.*/data/}, 'data/') }
+  json.delete("identification_format_version")
   json.to_json.md5
 end
 
