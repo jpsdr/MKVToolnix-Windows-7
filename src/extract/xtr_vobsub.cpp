0bca462cd8e3b7395f2e52d06a2fb8ee717e2378
Tom Yan <tom.ty89@gmail.com>
2022-08-21 19:23:12 +0800

    xtr_vobsub: use "--" instead of "en" as the fallback id
    as that is considered valid after commit 20252a2f

--- src/extract/xtr_vobsub.cpp
+++ src/extract/xtr_vobsub.cpp
@@ -274,8 +274,8 @@ void
 xtr_vobsub_c::write_idx(mm_io_c &idx,
                         int index) {
   auto language = mtx::iso639::look_up(m_language);
-  auto iso639_1 = language ? language->alpha_2_code : "en"s;
-  idx.puts(fmt::format("\nid: {0}, index: {1}\n", iso639_1.empty() ? "en"s : iso639_1, index));
+  auto iso639_1 = language ? language->alpha_2_code : "--"s;
+  idx.puts(fmt::format("\nid: {0}, index: {1}\n", iso639_1.empty() ? "--"s : iso639_1, index));
 
   size_t i;
   for (i = 0; i < m_positions.size(); i++) {
