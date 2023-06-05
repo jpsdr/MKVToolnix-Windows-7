a4ca0f68aa30b763ef2cc412412229d57c60c2f6
Moritz Bunkus <mo@bunkus.online>
2023-06-05 19:45:46 +0200

    fix compilation

--- src/mkvtoolnix-gui/header_editor/language_value_page.cpp
+++ src/mkvtoolnix-gui/header_editor/language_value_page.cpp
@@ -32,7 +32,7 @@ QWidget *
 LanguageValuePage::createInputControl() {
   m_originalValue   = m_element ? static_cast<EbmlString *>(m_element)->GetValue() : "eng";
   auto qOriginal    = Q(m_originalValue);
-  auto languageOnly = qOriginal.mid(0, qOriginal.indexOf(L'-'));
+  auto languageOnly = qOriginal.mid(0, qOriginal.indexOf(Q('-')));
   auto languageOpt  = mtx::iso639::look_up(to_utf8(languageOnly), true);
   auto currentValue = languageOpt ? languageOpt->alpha_3_code : m_originalValue;
 
