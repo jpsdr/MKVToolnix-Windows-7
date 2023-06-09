ffcc6627b2246e26d031c9706da397d69c2c26c1
jpsdr <jpsdr.psx@free.fr>
2023-06-09 17:55:17 +0200

    Tag version non official.
    Signed-off-by: jpsdr <jpsdr.psx@free.fr>

--- src/common/version.cpp
+++ src/common/version.cpp
@@ -26,7 +26,7 @@
 #include "common/strings/parsing.h"
 #include "common/version.h"
 
-constexpr auto VERSIONNAME = "Milliontown";
+constexpr auto VERSIONNAME = "Milliontown - Non official";
 
 version_number_t::version_number_t(const std::string &s)
   : valid{}
