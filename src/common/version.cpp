bd3d41ff17fbca436acbd98a4edee81eb4cf5784
jpsdr <jpsdr.psx@free.fr>
2023-12-13 19:28:31 +0100

    We are now entering the last stage...
    Signed-off-by: jpsdr <jpsdr.psx@free.fr>

--- src/common/version.cpp
+++ src/common/version.cpp
@@ -26,7 +26,7 @@
 #include "common/strings/parsing.h"
 #include "common/version.h"
 
-constexpr auto VERSIONNAME = "Milliontown - Non official";
+constexpr auto VERSIONNAME = "Twilight Zone - Non official";
 
 version_number_t::version_number_t(const std::string &s)
   : valid{}
