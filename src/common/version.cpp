87b7a2a84e4505aa3eb2f50cca6f7e41be85b5c7
jpsdr <jpsdr.psx@free.fr>
2024-03-11 19:20:38 +0100

    Fix missing ";".
    Signed-off-by: jpsdr <jpsdr.psx@free.fr>

--- src/common/version.cpp
+++ src/common/version.cpp
@@ -26,7 +26,7 @@
 #include "common/strings/parsing.h"
 #include "common/version.h"
 
-constexpr auto VERSIONNAME = "The final countdown - Non official"
+constexpr auto VERSIONNAME = "The final countdown - Non official";
 
 version_number_t::version_number_t(const std::string &s)
   : valid{}
