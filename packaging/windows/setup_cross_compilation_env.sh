480f101e857f11e6e5308f7ea6206524a0fa7d81
Moritz Bunkus <mo@bunkus.online>
2024-07-15 19:58:29 +0200

    set up MXE cross-environment: use `gcc13` plugin

--- packaging/windows/setup_cross_compilation_env.sh
+++ packaging/windows/setup_cross_compilation_env.sh
@@ -53,6 +53,7 @@ function update_mingw_cross_env {
   cat > settings.mk <<EOF
 MXE_TARGETS = ${HOST}
 MXE_USE_CCACHE =
+MXE_PLUGIN_DIRS += plugins/gcc13
 JOBS = ${PARALLEL}
 
 MKVTOOLNIX_DEPENDENCIES=gettext libiconv zlib boost flac ogg pthreads vorbis cmark libdvdread gmp
