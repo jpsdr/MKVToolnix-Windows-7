81e3276804d2975466ccd5ce709840f28ca8ca73
jpsdr <jpsdr.psx@free.fr>
2024-10-18 19:04:28 +0200

    Put back QT5 build.
    Signed-off-by: jpsdr <jpsdr.psx@free.fr>

--- packaging/windows/setup_cross_compilation_env.sh
+++ packaging/windows/setup_cross_compilation_env.sh
@@ -57,6 +57,7 @@ MXE_PLUGIN_DIRS += plugins/gcc13
 JOBS = ${PARALLEL}
 
 MKVTOOLNIX_DEPENDENCIES=gettext libiconv zlib boost flac ogg pthreads vorbis cmark libdvdread gmp
+MKVTOOLNIX_DEPENDENCIES+=qtbase qttranslations qtwinextras
 MKVTOOLNIX_DEPENDENCIES+=qt5 qtmultimedia
 
 LOCAL_PKG_LIST=\$(MKVTOOLNIX_DEPENDENCIES)
