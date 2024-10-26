902ee0ac1b4f7310396d6bdb010e1541a0dfa3da
Moritz Bunkus <mo@bunkus.online>
2024-10-26 19:16:12 +0200

    JSON parser: remove custom comment remover, use nlohmann's comment stripping

--- src/common/json.cpp
+++ src/common/json.cpp
@@ -34,72 +34,6 @@ enum class parser_state_e {
   in_comment,
 };
 
-nlohmann::json::string_t
-strip_comments(nlohmann::json::string_t const &data)  {
-  mm_text_io_c in{std::make_shared<mm_mem_io_c>(reinterpret_cast<uint8_t const *>(data.c_str()), data.length())};
-  mm_mem_io_c out{nullptr, data.length(), 100};
-
-  auto state = parser_state_e::normal;
-
-  in.set_byte_order_mark(byte_order_mark_e::utf8);
-
-  try {
-    while (!in.eof()) {
-      auto codepoint = in.read_next_codepoint();
-      if (codepoint.empty())
-        break;
-
-      if (state == parser_state_e::in_string_escaping) {
-        out.write(codepoint);
-        state = parser_state_e::in_string;
-        continue;
-      }
-
-      if (state == parser_state_e::in_string) {
-        out.write(codepoint);
-
-        state = codepoint == "\\" ? parser_state_e::in_string_escaping
-              : codepoint == "\"" ? parser_state_e::normal
-              :                     parser_state_e::in_string;
-
-        continue;
-      }
-
-      if (state == parser_state_e::in_comment) {
-        if ((codepoint == "\r") || (codepoint == "\n")) {
-          state = parser_state_e::normal;
-          out.write(codepoint);
-        }
-
-        continue;
-      }
-
-      if (codepoint == "/") {
-        try {
-          auto next_codepoint = in.read_next_codepoint();
-          if (next_codepoint == "/") {
-            state = parser_state_e::in_comment;
-            continue;
-          }
-
-          out.write(codepoint + next_codepoint);
-          continue;
-
-        } catch (mtx::mm_io::exception &) {
-        }
-
-      } else if (codepoint == "\"")
-        state = parser_state_e::in_string;
-
-      out.write(codepoint);
-    }
-
-  } catch (mtx::mm_io::exception &) {
-  }
-
-  return std::string{reinterpret_cast<char *>(out.get_buffer()), static_cast<std::string::size_type>(out.getFilePointer())};
-}
-
 void
 fix_invalid_utf8_recursively(nlohmann::json &json) {
   if (json.type() == nlohmann::json::value_t::string)
@@ -123,7 +57,7 @@ parse(nlohmann::json::string_t const &data,
   auto old_locale = std::string{::setlocale(LC_NUMERIC, "C")};
   at_scope_exit_c restore_locale{ [&old_locale]() { ::setlocale(LC_NUMERIC, old_locale.c_str()); } };
 
-  return nlohmann::json::parse(strip_comments(data), callback);
+  return nlohmann::json::parse(data, callback, true, true); // allow exceptions & comments
 }
 
 nlohmann::json::string_t
